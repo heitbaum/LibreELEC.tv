65198f1315675de3f89a6512c7d7ccb247392dba
heitbaum <6086324+heitbaum@users.noreply.github.com>
2020-11-18 10:17:54 +0000

    fluidsynth: update to 2.1.5

--- packages/audio/fluidsynth/package.mk
+++ packages/audio/fluidsynth/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fluidsynth"
-PKG_VERSION="1.1.6"
-PKG_SHA256="d28b47dfbf7f8e426902ae7fa2981d821fbf84f41da9e1b85be933d2d748f601"
+PKG_VERSION="2.1.5"
+PKG_SHA256="b539b7c65a650b56f01cd60a4e83c6125c217c5a63c0c214ef6274894a677d00"
 PKG_LICENSE="GPL"
 PKG_SITE="http://fluidsynth.org/"
-PKG_URL="$SOURCEFORGE_SRC/project/fluidsynth/fluidsynth-$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="https://github.com/FluidSynth/fluidsynth/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain glib"
 PKG_LONGDESC="FluidSynth renders midi music files as raw audio data, for playing or conversion."
 PKG_BUILD_FLAGS="+pic"
