a716534d002da45e06ff05f62d665094f640a519
heitbaum <6086324+heitbaum@users.noreply.github.com>
2020-11-18 10:17:54 +0000

    fluidsynth: update to 2.2.1
    update 1.1.6 (25 Jun 2017) to 2.2.1
    changelog: https://github.com/FluidSynth/fluidsynth/releases
    release notes:
    - https://github.com/FluidSynth/fluidsynth/releases/tag/v2.1.8
    - https://github.com/FluidSynth/fluidsynth/releases/tag/v2.2.0
    - https://github.com/FluidSynth/fluidsynth/releases/tag/v2.2.1

--- packages/audio/fluidsynth/package.mk
+++ packages/audio/fluidsynth/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fluidsynth"
-PKG_VERSION="1.1.6"
-PKG_SHA256="d28b47dfbf7f8e426902ae7fa2981d821fbf84f41da9e1b85be933d2d748f601"
+PKG_VERSION="2.2.1"
+PKG_SHA256="1c56660f23f6c406b36646cc619fc2d2a5265d1d3290e79bcef4505bcd985fdd"
 PKG_LICENSE="GPL"
 PKG_SITE="http://fluidsynth.org/"
-PKG_URL="${SOURCEFORGE_SRC}/project/fluidsynth/fluidsynth-${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://github.com/FluidSynth/fluidsynth/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain glib"
 PKG_LONGDESC="FluidSynth renders midi music files as raw audio data, for playing or conversion."
 PKG_BUILD_FLAGS="+pic"
