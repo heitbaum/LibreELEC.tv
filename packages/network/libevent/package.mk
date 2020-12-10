f3031ba7b0bb6055e25790d0f1f9df58a7c345f7
heitbaum <rudi@heitbaum.com>
2020-12-10 10:20:33 +0000

    libevent: used by lldpd to enable lldpd on network interface

---
+++ packages/network/libevent/package.mk
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libevent"
+PKG_VERSION="2.1.12-stable"
+PKG_SHA256="92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb"
+PKG_LICENSE="BSB-3c"
+PKG_SITE="https://github.com/libevent/libevent"
+PKG_URL="https://github.com/libevent/libevent/releases/download/release-$PKG_VERSION/libevent-$PKG_VERSION.tar.gz"
+PKG_LONGDESC="Event notification library"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_TOOLCHAIN="cmake"
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_CXX_STANDARD=14 -DCMAKE_CXX_EXTENSIONS:BOOL=OFF -DSPDLOG_FMT_EXTERNAL=ON -DSPDLOG_BUILD_EXAMPLE=OFF -DSPDLOG_BUILD_TESTS=OFF"
