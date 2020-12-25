a0d74b916fa4324c10fd393142860909c6520209
heitbaum <rudi@heitbaum.com>
2020-12-25 09:26:47 +0000

    runc:wip

--- packages/addons/addon-depends/runc/package.mk
+++ packages/addons/addon-depends/runc/package.mk
@@ -4,16 +4,18 @@
 
 PKG_NAME="runc"
 PKG_VERSION="1.0.0-rc92"
-PKG_SHA256="28378df983a3c586ed3ec8c76a774a9b10f36a0c323590a284b801cce95cc61f"
+PKG_VERSION="5efdc0c5a42d7965141271448dba6af4e1429962"
+#PKG_SHA256="28378df983a3c586ed3ec8c76a774a9b10f36a0c323590a284b801cce95cc61f"
 PKG_LICENSE="APL"
 PKG_SITE="https://github.com/opencontainers/runc"
 PKG_URL="https://github.com/opencontainers/runc/archive/v${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/opencontainers/runc/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain go:host"
 PKG_LONGDESC="A CLI tool for spawning and running containers according to the OCI specification."
 PKG_TOOLCHAIN="manual"
 
 # Git commit of the matching release https://github.com/opencontainers/runc/releases
-PKG_GIT_COMMIT="ff819c7e9184c13b7c2607fe6c30ae19403a7aff"
+PKG_GIT_COMMIT="5efdc0c5a42d7965141271448dba6af4e1429962"
 
 pre_make_target() {
   go_configure
