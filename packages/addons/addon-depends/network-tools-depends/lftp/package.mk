7d001045a66242667b64f87478f6898467e377de
heitbaum <rudi@heitbaum.com>
2020-12-30 06:33:25 +0000

    lftp: wip

--- packages/addons/addon-depends/network-tools-depends/lftp/package.mk
+++ packages/addons/addon-depends/network-tools-depends/lftp/package.mk
@@ -2,12 +2,13 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="lftp"
-PKG_VERSION="4.8.4"
-PKG_SHA256="4ebc271e9e5cea84a683375a0f7e91086e5dac90c5d51bb3f169f75386107a62"
+PKG_VERSION="4.9.2"
+PKG_SHA256="c517c4f4f9c39bd415d7313088a2b1e313b2d386867fe40b7692b83a20f0670d"
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://lftp.yar.ru/"
 PKG_URL="http://lftp.yar.ru/ftp/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain readline openssl zlib libidn2"
+PKG_DEPENDS_CONFIG="readline"
 PKG_LONGDESC="A sophisticated ftp/http client, and a file transfer program supporting a number of network protocols."
 PKG_BUILD_FLAGS="-sysroot"
 
