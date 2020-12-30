777a14667b0823fe6973ceb649487f3004813d23
heitbaum <rudi@heitbaum.com>
2020-12-30 12:04:59 +0000

    sshfs: wip update to 3.7.1

--- packages/addons/addon-depends/network-tools-depends/sshfs/package.mk
+++ packages/addons/addon-depends/network-tools-depends/sshfs/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="sshfs"
-PKG_VERSION="2.10"
-PKG_SHA256="70845dde2d70606aa207db5edfe878e266f9c193f1956dd10ba1b7e9a3c8d101"
+PKG_VERSION="3.7.1"
+PKG_SHA256="fe5d3436d61b46974889e0c4515899c21a9d67851e3793c209989f72353d7750"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libfuse/sshfs"
-PKG_URL="https://github.com/libfuse/sshfs/releases/download/sshfs-${PKG_VERSION}/sshfs-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/libfuse/sshfs/releases/download/sshfs-${PKG_VERSION}/sshfs-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain fuse glib"
 PKG_LONGDESC="A filesystem client based on the SSH File Transfer Protocol."
 PKG_BUILD_FLAGS="-sysroot"
