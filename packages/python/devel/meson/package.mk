6458cf30d28c22b3d620901dbd27fb0f6b442fd9
heitbaum <rudi@heitbaum.com>
2021-01-09 08:21:43 +0000

    meson: update to 0.56.2
    update from 0.56.0 (30 Oct 2020) to 0.56.2 (10 Jan 2021)
    changelogs:
    - https://github.com/mesonbuild/meson/milestone/55?closed=1
    - https://github.com/mesonbuild/meson/milestone/56?closed=1
    No release note, bug fix version

--- packages/python/devel/meson/package.mk
+++ packages/python/devel/meson/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="meson"
-PKG_VERSION="0.56.0"
-PKG_SHA256="291dd38ff1cd55fcfca8fc985181dd39be0d3e5826e5f0013bf867be40117213"
+PKG_VERSION="0.56.2"
+PKG_SHA256="3cb8bdb91383f7f8da642f916e4c44066a29262caa499341e2880f010edb87f4"
 PKG_LICENSE="Apache"
 PKG_SITE="http://mesonbuild.com"
 PKG_URL="https://github.com/mesonbuild/meson/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
