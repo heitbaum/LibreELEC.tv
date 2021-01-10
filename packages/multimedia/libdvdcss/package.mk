770d85ab3803969241a950e4806dd82151ba1f40
heitbaum <rudi@heitbaum.com>
2021-01-10 08:18:17 +0000

    libdvdcss: update to 1.4.3-Matrix-Beta-2

--- packages/multimedia/libdvdcss/package.mk
+++ packages/multimedia/libdvdcss/package.mk
@@ -3,8 +3,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libdvdcss"
-PKG_VERSION="1.4.2-Leia-Beta-5"
-PKG_SHA256="38816f8373e243bc5950449b4f3b18938c4e1c59348e3411e23f31db4072e40d"
+# From https://github.com/howie-f/libdvdcss/releases/tag/1.4.3-Matrix-Beta-2
+PKG_VERSION="bc00dd5770eea1b71209c279f7af43a66a5327fd"
+PKG_SHA256="976f373888dd782f74b97264799005a6817b43db3805e9d844b94980027a4fbe"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/xbmc/libdvdcss"
 PKG_URL="https://github.com/xbmc/libdvdcss/archive/${PKG_VERSION}.tar.gz"
