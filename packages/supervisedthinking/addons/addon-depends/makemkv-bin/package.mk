2c4e3842092d8999031af32fbc0533ea9db6640b
SupervisedThinking <supervisedthinking@gmail.com>
2021-01-19 17:26:29 +0100

    makemkv-bin: initial package

---
+++ packages/supervisedthinking/addons/addon-depends/makemkv-bin/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+
+PKG_NAME="makemkv-bin"
+PKG_VERSION="1.15.4"
+PKG_SHA256="45eba7e61a7b467b1fe8de722fa890d556e19f3fc02a7b7f8d846ac8e2badb9a"
+PKG_ARCH="x86_64"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.makemkv.com/"
+PKG_URL="http://www.makemkv.com/download/makemkv-bin-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="MakeMKV binaries & EULA"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  :
+}
