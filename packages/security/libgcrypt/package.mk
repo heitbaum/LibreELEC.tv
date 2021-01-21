effe9c73ebe902b656d9822cbf091acea9f00ce4
heitbaum <rudi@heitbaum.com>
2021-01-21 05:34:54 +0000

    libgcrypt: update to 1.10.1 WIP
    update 1.8.7 (2020-10-23) to 1.10.1 (2022-02-01)
    changelog:
    - https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgcrypt.git;a=blob_plain;f=NEWS
    - https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgcrypt.git;a=blob;f=NEWS;h=32b580a8d2823e066a59853ac2ae59a3b7b610a5;hb=05422ca24a0391dad2a0b7790a904ce348819c10
    
    need to review this - http://lists.busybox.net/pipermail/buildroot/2019-July/253921.html
    in relation to the removal of the patch.

--- packages/security/libgcrypt/package.mk
+++ packages/security/libgcrypt/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libgcrypt"
-PKG_VERSION="1.8.7"
-PKG_SHA256="03b70f028299561b7034b8966d7dd77ef16ed139c43440925fe8782561974748"
+PKG_VERSION="1.10.1"
+PKG_SHA256="ef14ae546b0084cd84259f61a55e07a38c3b53afc0f546bffcef2f01baffe9de"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://www.gnupg.org/"
 PKG_URL="https://www.gnupg.org/ftp/gcrypt/libgcrypt/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
