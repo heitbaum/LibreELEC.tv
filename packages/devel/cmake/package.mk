f7a15a771fe3fb016bf283677b54f98581025035
heitbaum <rudi@heitbaum.com>
2021-01-21 07:35:42 +0000

    cmake: update to 3.19.3

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="cmake"
-PKG_VERSION="3.19.2"
-PKG_SHA256="e3e0fd3b23b7fb13e1a856581078e0776ffa2df4e9d3164039c36d3315e0c7f0"
+PKG_VERSION="3.19.3"
+PKG_SHA256="3faca7c131494a1e34d66e9f8972ff5369e48d419ea8ceaa3dc15b4c11367732"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cmake.org/"
 PKG_URL="https://cmake.org/files/v$(get_pkg_version_maj_min)/cmake-${PKG_VERSION}.tar.gz"
