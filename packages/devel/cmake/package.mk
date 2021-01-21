22dfcf2c6479da9a936240c6a2e39b59d3a46f0a
heitbaum <rudi@heitbaum.com>
2021-01-21 07:35:42 +0000

    cmake: update to 3.19.6
    update 3.19.2 to 3.19.6
    release notes:
    - https://cmake.org/cmake/help/latest/release/3.19.html#id6

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="cmake"
-PKG_VERSION="3.19.2"
-PKG_SHA256="e3e0fd3b23b7fb13e1a856581078e0776ffa2df4e9d3164039c36d3315e0c7f0"
+PKG_VERSION="3.19.6"
+PKG_SHA256="ec87ab67c45f47c4285f204280c5cde48e1c920cfcfed1555b27fb3b1a1d20ba"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cmake.org/"
 PKG_URL="https://cmake.org/files/v$(get_pkg_version_maj_min)/cmake-${PKG_VERSION}.tar.gz"
