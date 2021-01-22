5a7a09976073501d66ae74c145a0356ae21e26fa
heitbaum <rudi@heitbaum.com>
2021-01-22 11:41:39 +0000

    gdk-pixbuf: WIP

--- packages/addons/addon-depends/chrome-depends/gdk-pixbuf/package.mk
+++ packages/addons/addon-depends/chrome-depends/gdk-pixbuf/package.mk
@@ -8,6 +8,7 @@ PKG_SHA256="83c66a1cfd591d7680c144d2922c5955d38b4db336d7cd3ee109f7bcf9afef15"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.gtk.org/"
 PKG_URL="https://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/${PKG_VERSION:0:4}/gdk-pixbuf-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_HOST="shared-mime-info:host"
 PKG_DEPENDS_TARGET="toolchain glib libjpeg-turbo libpng jasper shared-mime-info tiff libX11"
 PKG_DEPENDS_CONFIG="shared-mime-info"
 PKG_LONGDESC="GdkPixbuf is a a GNOME library for image loading and manipulation."
@@ -16,3 +17,8 @@ PKG_MESON_OPTS_TARGET="-Ddocs=false \
                        -Dgir=false \
                        -Dman=false \
                        -Drelocatable=false"
+
+PKG_MESON_OPTS_HOST="-Ddocs=false \
+                     -Dgir=false \
+                     -Dman=false \
+                     -Drelocatable=false"
