3811be39389b70784451968bddeb2f3f35717dbe
heitbaum <rudi@heitbaum.com>
2021-01-28 12:57:29 +0000

    jdk-x86_64-zulu: update to 11.45.27-11.0.10
    update 8.38.0.13-8.0.212 to 11.45.27-11.0.10 (2021-01-19)
    
    ref that openjdk 11 is supported by libbluray 1.1.0
    - https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=893227#101
    
    changelog (libbluray support):
    - https://code.videolan.org/videolan/libbluray/-/blob/master/ChangeLog
    - 2019-02-12: Version 1.1.0
      - Add initial support for OpenJDK 11.
    
    release notes:
    - https://docs.azul.com/zulu/zulurelnotes/ZuluReleaseNotes/Title.htm
    
    update the package to enable building jre (cross-target):
    - https://www.oracle.com/java/technologies/javase/jdk-11-relnote.html
        Installing the JDK in previous releases optionally
        installed a JRE. In JDK 11, this is no longer an option. In this
        release, the JRE or Server JRE is no longer offered. Only the JDK is
        offered. Users can use jlink to create smaller custom runtimes.

--- packages/addons/addon-depends/jre-depends/jdk-x86_64-zulu/package.mk
+++ packages/addons/addon-depends/jre-depends/jdk-x86_64-zulu/package.mk
@@ -1,10 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Peter Vicman (peter.vicman@gmail.com)
+# Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="jdk-x86_64-zulu"
-PKG_VERSION="8.84.0.15-8.0.442"
-PKG_SHA256="6e3bd4d911e6eb2d14e0b48e622b6909c76add0b51c51d11f5c2c3d2a045bcf3"
+PKG_VERSION="11.45.27-11.0.10"
+PKG_SHA256="0bd85593bae021314378f3b146cfe36a6c9b0afd964d897c34201034ace3e785"
 PKG_LICENSE="GPLv2"
+if [ ! "${HOSTTYPE}" = "${TARGET_ARCH}" ]; then
+  PKG_DEPENDS_UNPACK="jdk-${HOSTTYPE}-zulu"
+fi
 PKG_SITE="https://www.azul.com/products/zulu-enterprise/"
 PKG_URL="https://cdn.azul.com/zulu/bin/zulu${PKG_VERSION%%-*}-ca-jdk${PKG_VERSION##*-}-linux_x64.tar.gz"
 PKG_LONGDESC="Zulu, the open Java(TM) platform from Azul Systems."
@@ -12,4 +16,8 @@ PKG_TOOLCHAIN="manual"
 
 post_unpack() {
   rm -f ${PKG_BUILD}/src.zip
+
+  # Create a runtime image with all Java SE modules
+  cd ${PKG_BUILD}
+    bin/jlink --output jre11 --add-modules java.se
 }
