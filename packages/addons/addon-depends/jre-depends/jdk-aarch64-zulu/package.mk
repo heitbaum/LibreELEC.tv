60e97720b90cbf2b0235ee92c765247f541ef885
heitbaum <rudi@heitbaum.com>
2021-01-28 12:41:22 +0000

    jdk-aarch64-zulu: update to 11.45.27-11.0.10
    update 8.38.0.162-1.8.0_212 to 11.45.27-11.0.10 (2021-01-19)
    
    ref that openjdk 11 is supported by libbluray 1.1.0
    - https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=893227#101
    
    changelog (libbluray support):
    - https://code.videolan.org/videolan/libbluray/-/blob/master/ChangeLog
    - 2019-02-12: Version 1.1.0
      - Add initial support for OpenJDK 11.
    
    release notes:
    - https://docs.azul.com/zulu/zulurelnotes/ZuluReleaseNotes/Title.htm
    
    update the package to enable building jre (cross-target):
    - https://www.oracle.com/java/technologies/javase/jdk-11-relnote.html
        Installing the JDK in previous releases optionally
        installed a JRE. In JDK 11, this is no longer an option. In this
        release, the JRE or Server JRE is no longer offered. Only the JDK is
        offered. Users can use jlink to create smaller custom runtimes.

--- packages/addons/addon-depends/jre-depends/jdk-aarch64-zulu/package.mk
+++ packages/addons/addon-depends/jre-depends/jdk-aarch64-zulu/package.mk
@@ -1,10 +1,14 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Peter Vicman (peter.vicman@gmail.com)
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="jdk-aarch64-zulu"
-PKG_VERSION="8.38.0.162-1.8.0_212"
-PKG_SHA256="2afa6b9a86fea6f9275856506b5cc1efd8420f674c5e2dc3e1b04e140d6ad852"
+PKG_VERSION="11.45.27-11.0.10"
+PKG_SHA256="e5d211df9576040919eb22884189b59f50cd319877c8855e37714eff1b327d2b"
 PKG_LICENSE="GPLv2"
+if [ ! "${HOSTTYPE}" = "${TARGET_ARCH}" ]; then
+  PKG_DEPENDS_UNPACK="jdk-${HOSTTYPE}-zulu"
+fi
 PKG_SITE="https://www.azul.com/products/zulu-embedded/"
 PKG_URL="http://cdn.azul.com/zulu-embedded/bin/zulu${PKG_VERSION%%-*}-ca-jdk${PKG_VERSION##*-}-linux_aarch64.tar.gz"
 PKG_LONGDESC="Zulu, the open Java(TM) platform from Azul Systems."
@@ -12,4 +16,8 @@ PKG_TOOLCHAIN="manual"
 
 post_unpack() {
   rm -f ${PKG_BUILD}/src.zip
+
+  # Create a runtime image with all Java SE modules
+  cd $(get_build_dir jdk-${HOSTTYPE}-zulu) # this is the buildhost.ARCH
+    bin/jlink --module-path ${PKG_BUILD}/jmods --output ${PKG_BUILD}/jre11 --add-modules java.se
 }
