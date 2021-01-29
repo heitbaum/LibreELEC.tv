ff7e930a3849312886463decb3894c239f59adbe
heitbaum <rudi@heitbaum.com>
2021-01-29 10:57:11 +0000

    pngquant: update to 2.14.0
    update 2.13.1 to 2.14.0
    changelog: https://github.com/kornelski/pngquant/blob/2.14.0/CHANGELOG

--- packages/addons/addon-depends/pngquant/package.mk
+++ packages/addons/addon-depends/pngquant/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pngquant"
-PKG_VERSION="2.13.1"
+PKG_VERSION="2.14.0"
 PKG_SHA256="4b911a11aa0c35d364b608c917d13002126185c8c314ba4aa706b62fd6a95a7a"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://pngquant.org"
