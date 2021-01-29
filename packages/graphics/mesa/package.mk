210a6e8a6679e8575005793e1e70536079b0467d
heitbaum <rudi@heitbaum.com>
2021-01-29 10:18:52 +0000

    mesa: enable zstd compression for mesa shader cache
    mesa commit (2019):
    - https://cgit.freedesktop.org/mesa/mesa/commit/?id=a8d941091f72923561a6c58b46ccb264b6a0e205

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="565c6f4bd2d5747b919454fc1d439963024fc78ca56fd05158c3b2cde2f6912b"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain expat libdrm Mako:host"
+PKG_DEPENDS_TARGET="toolchain expat libdrm zstd Mako:host"
 PKG_LONGDESC="Mesa is a 3-D graphics library with an API."
 PKG_TOOLCHAIN="meson"
 
