c1e9468361a461aacbefc871a7a60aebeb75ba37
heitbaum <rudi@heitbaum.com>
2021-02-04 15:22:21 +0000

    Revert "vdpauinfo: update to 1.4"
    This reverts commit a725c2da7ea2b3435410ea1d85e79bdf1d846e82.

--- packages/debug/vdpauinfo/package.mk
+++ packages/debug/vdpauinfo/package.mk
@@ -2,11 +2,10 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="vdpauinfo"
-PKG_VERSION="1.4"
-PKG_SHA256="50d4326725d62fe1948b0236b96cfc753b035e406f4e6496508a76fbfdecc390"
+PKG_VERSION="1.0"
+PKG_SHA256="4054960b7ae618c351ff1ce3e7831b5cbda964ae1fbf9969b7146404d3044bc4"
 PKG_LICENSE="GPL"
 PKG_SITE="http://freedesktop.org/wiki/Software/VDPAU"
-# https://gitlab.freedesktop.org/vdpau/vdpauinfo/uploads/8f047eac351672cc4316724edb6ad2b2/vdpauinfo-1.4.tar.gz
-PKG_URL="https://gitlab.freedesktop.org/vdpau/vdpauinfo/-/archive/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_URL="http://people.freedesktop.org/~aplattner/vdpau/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libvdpau"
 PKG_LONGDESC="A tool to show vdpau infos."
