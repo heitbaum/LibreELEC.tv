cdfdfb9594c47d7a789d8e55015261f948c4d28e
heitbaum <rudi@heitbaum.com>
2021-02-17 07:46:50 +0000

    intel-ucode: update to 20210216
    update 20201118 to 20210216
    release notes: https://github.com/intel/Intel-Linux-Processor-Microcode-Data-Files/releases/tag/microcode-20210216

--- packages/linux-firmware/intel-ucode/package.mk
+++ packages/linux-firmware/intel-ucode/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="intel-ucode"
-PKG_VERSION="20201118"
-PKG_SHA256="e42a264b7b86e80d013d6d00062467352c1f37e0aaea10fe5b51e4d8687921ab"
+PKG_VERSION="20210216"
+PKG_SHA256="b855c81f78705f35341248a0603aa1a6e199ca7f59cd425e061b579329aa9eaa"
 PKG_ARCH="x86_64"
 PKG_LICENSE="other"
 PKG_SITE="https://downloadcenter.intel.com/search?keyword=linux+microcode"
