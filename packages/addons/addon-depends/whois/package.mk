83ed9ed8e5b93a0a7522733e3009a554a77605ff
heitbaum <rudi@heitbaum.com>
2021-02-17 07:54:20 +0000

    whois: update to 5.5.8

--- packages/addons/addon-depends/whois/package.mk
+++ packages/addons/addon-depends/whois/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="whois"
-PKG_VERSION="5.5.7"
-PKG_SHA256="ae389c1486cdeae99f5f02940f98f5d7ff1f08ac0f96810365159b27b0189b5e"
+PKG_VERSION="5.5.8"
+PKG_SHA256="e227dfc912271ebff5bc144782a8da680afed729f68e71d317591c33d6ae0eed"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.linux.it/~md/software/"
 PKG_URL="https://github.com/rfc1036/whois/archive/v${PKG_VERSION}.tar.gz"
