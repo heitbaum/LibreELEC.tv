299599153112d611dffb2f83733f59791ed3ed2c
heitbaum <rudi@heitbaum.com>
2021-02-26 09:47:48 +0000

    pcsc-lite: update to 1.9.1
    update 1.9.0 (2020-06-14) to 1.9.1 (2021-02-16)
    changelog: https://salsa.debian.org/rousseau/PCSC/blob/master/ChangeLog
    diff: https://salsa.debian.org/rousseau/PCSC/-/compare/1.9.0...1.9.1

--- packages/addons/addon-depends/pcsc-lite/package.mk
+++ packages/addons/addon-depends/pcsc-lite/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pcsc-lite"
-PKG_VERSION="1.9.0"
-PKG_SHA256="0148d403137124552c5d0f10f8cdab2cbb8dfc7c6ce75e018faf667be34f2ef9"
+PKG_VERSION="1.9.1"
+PKG_SHA256="73c4789b7876a833a70f493cda21655dfe85689d9b7e29701c243276e55e683a"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pcsclite.apdu.fr"
 PKG_URL="https://pcsclite.apdu.fr/files/pcsc-lite-${PKG_VERSION}.tar.bz2"
