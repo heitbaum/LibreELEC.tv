f0d73ee6791feebdd75f964b55ed8bd47507bd54
heitbaum <rudi@heitbaum.com>
2021-02-26 10:17:51 +0000

    liberation-fonts-ttf: update to 2.1.3
    update 2.1.2 (2020-12-21) to 2.1.3 (2021-02-23)
    release notes: https://github.com/liberationfonts/liberation-fonts/releases/tag/2.1.3

--- packages/x11/font/liberation-fonts-ttf/package.mk
+++ packages/x11/font/liberation-fonts-ttf/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="liberation-fonts-ttf"
-PKG_VERSION="2.1.2"
-PKG_SHA256="14694930f28391675008c67b18889d1a7dfea74b16adf50394f8057b57eaf8e0"
+PKG_VERSION="2.1.3"
+PKG_SHA256="8947bb7c0ad4d0d25b6ae93d40ec5fbe3dbbd4de21b81a62aa0c41a801e36fdd"
 PKG_LICENSE="OFL1_1"
 PKG_SITE="https://github.com/liberationfonts/liberation-fonts"
-PKG_URL="https://github.com/liberationfonts/liberation-fonts/files/5722233/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/liberationfonts/liberation-fonts/files/6026893/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain util-macros"
 PKG_LONGDESC="This packages included the high-quality and open-sourced TrueType vector fonts."
 PKG_TOOLCHAIN="manual"
