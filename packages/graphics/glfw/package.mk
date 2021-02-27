65939bd14314e575a372338a30d05b483d25f99d
heitbaum <rudi@heitbaum.com>
2021-02-27 09:34:30 +0000

    WIP glfw: update to 3.3.3
    This is a bug fix release. It adds basic support for macOS 11.0 and adds
    fixes for issues on all supported platforms.
    
    update 3.3.2 (2021-01-20) to 3.3.3 (2021-02-23)
    changelog: https://www.glfw.org/changelog.html
    tag: https://github.com/glfw/glfw/releases/tag/3.3.3

--- packages/graphics/glfw/package.mk
+++ packages/graphics/glfw/package.mk
@@ -2,12 +2,13 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="glfw"
-PKG_VERSION="3.3.2"
-PKG_SHA256="aa9a2325c3d2cb3a18b236326b3f866545a46cd48514b00c999fdd9374a7325b"
+PKG_VERSION="3.3.3"
+PKG_SHA256="aa9922b55a464d5bab58fcbe5a619f517d54e3dc122361c116de573670006a7a"
 PKG_ARCH="x86_64"
 PKG_LICENSE="BSD"
 PKG_SITE="http://glfw.org"
-PKG_URL="$SOURCEFORGE_SRC/glfw/glfw-$PKG_VERSION.tar.gz"
+#PKG_URL="$SOURCEFORGE_SRC/glfw/glfw-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/${PKG_NAME}/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain mesa glu libXcursor libXinerama libXi"
 PKG_LONGDESC="provides a simple API for creating windows, contexts and surfaces, receiving input and events"
 
