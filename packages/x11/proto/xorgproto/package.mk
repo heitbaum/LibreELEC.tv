185247236f98e932e0fd7522ce8de1e0541c07ad
heitbaum <rudi@heitbaum.com>
2021-02-27 11:41:57 +0000

    xorgproto: update to 2021.3
    update 2020.1 to 2021.3
    announce: https://www.mail-archive.com/xorg-announce@lists.x.org/msg01281.html
    announce: https://www.mail-archive.com/xorg-announce@lists.x.org/msg01282.html
    diff: https://gitlab.freedesktop.org/xorg/proto/xorgproto/-/compare/xorgproto-2020.1...xorgproto-2021.3

--- packages/x11/proto/xorgproto/package.mk
+++ packages/x11/proto/xorgproto/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xorgproto"
-PKG_VERSION="2020.1"
-PKG_SHA256="54a153f139035a376c075845dd058049177212da94d7a9707cf9468367b699d2"
+PKG_VERSION="2021.3"
+PKG_SHA256="4c732b14fc7c7db64306374d9e8386d6172edbb93f587614df1938b9d9b9d737"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
 PKG_URL="https://xorg.freedesktop.org/archive/individual/proto/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
