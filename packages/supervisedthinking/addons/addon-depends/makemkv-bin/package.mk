2f291f1e9742c298accdea27a22552b9924ae9a9
heitbaum <rudi@heitbaum.com>
2021-03-01 11:53:51 +0000

    makemkv-bin: update to 1.16.0
    forum post: https://www.makemkv.com/forum/viewtopic.php?f=3&t=224
    history: https://www.makemkv.com/download/history.html

--- packages/supervisedthinking/addons/addon-depends/makemkv-bin/package.mk
+++ packages/supervisedthinking/addons/addon-depends/makemkv-bin/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="makemkv-bin"
-PKG_VERSION="1.15.4"
-PKG_SHA256="45eba7e61a7b467b1fe8de722fa890d556e19f3fc02a7b7f8d846ac8e2badb9a"
-PKG_ARCH="x86_64"
+PKG_VERSION="1.16.0"
+PKG_SHA256="96b4f1cd8b7b96e50e5ebbad51e983fcf0b8ffba06df38c74a2777f6bd61c172"
+PKG_ARCH="aarch64 arm x86_64"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.makemkv.com/"
 PKG_URL="http://www.makemkv.com/download/makemkv-bin-${PKG_VERSION}.tar.gz"
