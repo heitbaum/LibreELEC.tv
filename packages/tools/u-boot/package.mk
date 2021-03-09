0b59db8eb28ee3c82c75767b4b4deb261ae50c54
Christian Hewitt <christianshewitt@gmail.com>
2021-03-09 07:25:20 +0000

    u-boot: bump to 2021.04 with WIP patches

--- packages/tools/u-boot/package.mk
+++ packages/tools/u-boot/package.mk
@@ -23,9 +23,10 @@ PKG_NEED_UNPACK="${PROJECT_DIR}/${PROJECT}/bootloader"
 
 case "${PROJECT}" in
   Amlogic)
-    PKG_VERSION="807482107a6d426dbcd6457d9ccf8b3ce6ca887b" # 2021.04-rc2 custodians/u-boot-amlogic-test
-    PKG_SHA256="a10430d2c1a1d9e83e66bed342433ddfe4f3d6f16d9fa8b4d4c034b600baffd3"
-    PKG_URL="https://github.com/chewitt/u-boot/archive/${PKG_VERSION}.tar.gz"
+    PKG_VERSION="2021.04"
+    PKG_SHA256="0d438b1bb5cceb57a18ea2de4a0d51f7be5b05b98717df05938636e0aadfe11a"
+    #PKG_URL="https://github.com/chewitt/u-boot/archive/${PKG_VERSION}.tar.gz"
+    PKG_URL="http://ftp.denx.de/pub/u-boot/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
     PKG_PATCH_DIRS="amlogic"
     ;;
   Rockchip)
