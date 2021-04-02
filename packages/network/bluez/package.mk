7b7a53cd2c796ee53496575bd0b7af7df6ac1b9a
heitbaum <rudi@heitbaum.com>
2021-04-02 12:08:08 +0000

    bluez: update to 5.57
    update 5.56 (2021-02-22) to 5.57 (2021-04-02)
    log: https://git.kernel.org/pub/scm/bluetooth/bluez.git/log/

--- packages/network/bluez/package.mk
+++ packages/network/bluez/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bluez"
-PKG_VERSION="5.56"
-PKG_SHA256="59c4dba9fc8aae2a6a5f8f12f19bc1b0c2dc27355c7ca3123eed3fe6bd7d0b9d"
+PKG_VERSION="5.57"
+PKG_SHA256="73b258d965769dcfdb7a27c5c9b4c9a8b3f72635e53278b73313c8742cf0f69c"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.bluez.org/"
 PKG_URL="https://www.kernel.org/pub/linux/bluetooth/${PKG_NAME}-${PKG_VERSION}.tar.xz"
