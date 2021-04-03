aa2fa0dab2f8a1d15214b042d6c1a42ecbb1a86e
heitbaum <rudi@heitbaum.com>
2021-04-03 09:20:27 +0000

    Pillow: update to 8.2.0

--- packages/python/graphics/Pillow/package.mk
+++ packages/python/graphics/Pillow/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="Pillow"
-PKG_VERSION="8.1.2"
-PKG_SHA256="4b99c0a07e8bc4048b4f37ee515d02cc2f895453afe534e4b00bfe2f2a2dbe39"
+PKG_VERSION="8.2.0"
+PKG_SHA256="da86280bbfcd2b7fd87d33ce6f28688bf600600dfdbe85bfc16c562bbd26ca97"
 PKG_LICENSE="BSD"
 PKG_SITE="https://python-pillow.org/"
 PKG_URL="https://github.com/python-pillow/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
