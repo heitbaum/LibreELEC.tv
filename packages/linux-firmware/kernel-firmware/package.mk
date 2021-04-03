4015e1457901b97d5beaea1efa7b8ad545fbb561
heitbaum <rudi@heitbaum.com>
2021-04-03 09:33:39 +0000

    kernel-firmware: update to 20210315
    update 20210208 to 20210315

--- packages/linux-firmware/kernel-firmware/package.mk
+++ packages/linux-firmware/kernel-firmware/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kernel-firmware"
-PKG_VERSION="20210208"
-PKG_SHA256="45ae017429f60e701ece1e8cd0271e2cd3fc4400e7505856833b08f8a84a1bd9"
+PKG_VERSION="20210315"
+PKG_SHA256="c6b9decef3e94a8b6c9810579bb11887123c479442dd3c586823848b2f363caf"
 PKG_LICENSE="other"
 PKG_SITE="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/"
 PKG_URL="https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/snapshot/${PKG_VERSION}.tar.gz"
