5896fd32c268ae4369e2aea7e3cb96837ab826a8
heitbaum <rudi@heitbaum.com>
2021-04-05 10:11:33 +0000

    oneDNN: initial package

---
+++ packages/devel/oneDNN/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="oneDNN"
+PKG_VERSION="2.5"
+PKG_SHA256="d7a47caeb28d2c67dc8fa0d0f338b11fbf25b473a608f04cfed913aea88815a9"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/oneapi-src/oneDNN"
+PKG_URL="https://github.com/oneapi-src/oneDNN/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="oneAPI Deep Neural Network Library (oneDNN)"
