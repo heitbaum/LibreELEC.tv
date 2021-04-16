f330e87f4e731cf184d07fb33634200c7eeb6ce8
heitbaum <rudi@heitbaum.com>
2021-04-16 19:51:34 +0000

    squashfs-tools: update to 4.4-git.1
    update from 4.4 to 4.4-git.1
    commits: https://github.com/plougher/squashfs-tools/compare/4.4...4.4-git.1
    
    incorporates gcc10 patch

--- packages/sysutils/squashfs-tools/package.mk
+++ packages/sysutils/squashfs-tools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="squashfs-tools"
-PKG_VERSION="4.4"
-PKG_SHA256="a7fa4845e9908523c38d4acf92f8a41fdfcd19def41bd5090d7ad767a6dc75c3"
+PKG_VERSION="4.4-git.1"
+PKG_SHA256="9b89c3d2f0d0456e99fa3dd4429d45044044f0115acb0f15c441a4b9496d25a7"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/plougher/squashfs-tools"
 PKG_URL="https://github.com/plougher/squashfs-tools/archive/${PKG_VERSION}.tar.gz"
