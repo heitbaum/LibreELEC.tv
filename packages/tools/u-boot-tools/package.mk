fdffcfd59c5a0af13af14bcc7f73386825aa2065
heitbaum <rudi@heitbaum.com>
2021-04-17 08:24:54 +0000

    u-boot-tools: update to 2021.04

--- packages/tools/u-boot-tools/package.mk
+++ packages/tools/u-boot-tools/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="u-boot-tools"
-PKG_VERSION="2021.01"
-PKG_SHA256="b407e1510a74e863b8b5cb42a24625344f0e0c2fc7582d8c866bd899367d0454"
+PKG_VERSION="2021.04"
+PKG_SHA256="0d438b1bb5cceb57a18ea2de4a0d51f7be5b05b98717df05938636e0aadfe11a"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
 PKG_URL="http://ftp.denx.de/pub/u-boot/u-boot-${PKG_VERSION}.tar.bz2"
