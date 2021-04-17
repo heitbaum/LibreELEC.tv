a818a053b532e1fabaff7c788f0edca58c19114d
heitbaum <rudi@heitbaum.com>
2021-04-17 02:44:22 +0000

    mtools: update to 4.0.27

--- packages/tools/mtools/package.mk
+++ packages/tools/mtools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mtools"
-PKG_VERSION="4.0.26"
-PKG_SHA256="539f1c8b476a16e198d8bcb10a5799e22e69de49d854f7dbd85b64c2a45dea1a"
+PKG_VERSION="4.0.27"
+PKG_SHA256="9c88c8d9244e54768e40b92c005e61ec4e084ea7c070df9184caf1a6408838b3"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/mtools/"
 PKG_URL="http://ftpmirror.gnu.org/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
