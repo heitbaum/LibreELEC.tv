ecc934d2040aba48d790b91988c526953d096f04
heitbaum <rudi@heitbaum.com>
2021-04-20 15:17:02 +0000

    tiff: update to 4.3.0

--- packages/graphics/tiff/package.mk
+++ packages/graphics/tiff/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tiff"
-PKG_VERSION="4.2.0"
-PKG_SHA256="eb0484e568ead8fa23b513e9b0041df7e327f4ee2d22db5a533929dfc19633cb"
+PKG_VERSION="4.3.0"
+PKG_SHA256="0e46e5acb087ce7d1ac53cf4f56a09b221537fc86dfc5daaad1c2e89e1b37ac8"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.remotesensing.org/libtiff/"
 PKG_URL="http://download.osgeo.org/libtiff/${PKG_NAME}-${PKG_VERSION}.tar.gz"
