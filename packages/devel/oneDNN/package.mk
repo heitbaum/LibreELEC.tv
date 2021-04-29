cabf177debf3b7b460cf9c0c6bb7971816a2ef9b
heitbaum <rudi@heitbaum.com>
2021-04-29 11:59:26 +0000

    oneDNN: update to 2.2.2

--- packages/devel/oneDNN/package.mk
+++ packages/devel/oneDNN/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="oneDNN"
-PKG_VERSION="2.2.1"
-PKG_SHA256="3faf3b7811dd37835169a9d5c57f17fd591f062029851186f9b11db117b9d1d9"
+PKG_VERSION="2.2.2"
+PKG_SHA256="01e651eb20ca1943bbdf756804fd02d13e5ff3c84b89e3aa5d40abdeb5bb07ee"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/oneapi-src/oneDNN"
 PKG_URL="https://github.com/oneapi-src/oneDNN/archive/v${PKG_VERSION}.tar.gz"
