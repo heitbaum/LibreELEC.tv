412b86417bdbac328a8c38e07f7f89b11949c077
heitbaum <rudi@heitbaum.com>
2021-04-30 09:53:41 +0000

    Revert "gcc: update to 11.1.0"
    This reverts commit b2d4397dc1800c25f17251146ca12a4dbf54a02d.

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gcc"
-PKG_VERSION="11.1.0"
-PKG_SHA256="4c4a6fb8a8396059241c2e674b85b351c26a5d678274007f076957afa1cc9ddf"
+PKG_VERSION="10.3.0"
+PKG_SHA256="64f404c1a650f27fc33da242e1f2df54952e3963a49e06e73f6940f3223ac344"
 PKG_LICENSE="GPL"
 PKG_SITE="http://gcc.gnu.org/"
 PKG_URL="http://ftpmirror.gnu.org/gcc/${PKG_NAME}-${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
