d5ea066f9579498403ea8d9d17017f50db897df7
heitbaum <rudi@heitbaum.com>
2021-04-30 06:38:04 +0000

    xorgproto: update to 2021.4

--- packages/x11/proto/xorgproto/package.mk
+++ packages/x11/proto/xorgproto/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xorgproto"
-PKG_VERSION="2021.3"
-PKG_SHA256="4c732b14fc7c7db64306374d9e8386d6172edbb93f587614df1938b9d9b9d737"
+PKG_VERSION="2021.4"
+PKG_SHA256="0f5157030162844b398e7ce69b8bb967c2edb8064b0a9c9bb5517eb621459fbf"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.X.org"
 PKG_URL="https://xorg.freedesktop.org/archive/individual/proto/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
