f1c4ad98f771b5c5a1f70bada4045e9a5eba1428
heitbaum <rudi@heitbaum.com>
2021-04-30 10:13:38 +0000

    libinput: update to 1.17.2

--- packages/wayland/libinput/package.mk
+++ packages/wayland/libinput/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libinput"
-PKG_VERSION="1.17.1"
-PKG_SHA256="e51c50f3ce253961bed452d9f93102cc26128406975ab88ff9ec9e6c3b875137"
+PKG_VERSION="1.17.2"
+PKG_SHA256="b822263086b6588b9a9a153be97dea409f63927fb67b9a241748e76f222a5be1"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/libinput/"
 PKG_URL="http://www.freedesktop.org/software/libinput/${PKG_NAME}-${PKG_VERSION}.tar.xz"
