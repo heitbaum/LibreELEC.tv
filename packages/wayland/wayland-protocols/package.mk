9b14b845be76b9ef511edd2213fe190ce8f1196f
heitbaum <rudi@heitbaum.com>
2021-05-01 05:06:39 +0000

    wayland-protocols: update to 1.21
    update 1.20 to 1.21
    log: https://github.com/wayland-project/wayland-protocols/compare/1.20...1.21
    
    The 1.21 uses meson (which is a higher priority a in build, but forces
    wayland-scanner.)
    
    ticket raised: https://gitlab.freedesktop.org/wayland/wayland-protocols/-/issues/45

--- packages/wayland/wayland-protocols/package.mk
+++ packages/wayland/wayland-protocols/package.mk
@@ -2,13 +2,15 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wayland-protocols"
-PKG_VERSION="1.20"
-PKG_SHA256="9782b7a1a863d82d7c92478497d13c758f52e7da4f197aa16443f73de77e4de7"
+PKG_VERSION="1.21"
+PKG_SHA256="b99945842d8be18817c26ee77dafa157883af89268e15f4a5a1a1ff3ffa4cde5"
 PKG_LICENSE="OSS"
 PKG_SITE="https://wayland.freedesktop.org/"
 PKG_URL="https://wayland.freedesktop.org/releases/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Specifications of extended Wayland protocols"
+#to be removed went version updated to allow meson build without wayland-scanner
+PKG_TOOLCHAIN="configure"
 
 post_makeinstall_target() {
   rm -rf ${INSTALL}
