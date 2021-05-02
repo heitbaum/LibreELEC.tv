04f6184e06da96c57c8aa1176b3fae446d1adc7a
heitbaum <rudi@heitbaum.com>
2021-05-02 14:04:50 +0000

    libass: update to 0.15.1
    update 0.15.0 (2020-10-27) to 0.15.1 (2021-05-02)
    release notes: https://github.com/libass/libass/releases

--- packages/multimedia/libass/package.mk
+++ packages/multimedia/libass/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libass"
-PKG_VERSION="0.15.0"
-PKG_SHA256="9f09230c9a0aa68ef7aa6a9e2ab709ca957020f842e52c5b2e52b801a7d9e833"
+PKG_VERSION="0.15.1"
+PKG_SHA256="1cdd39c9d007b06e737e7738004d7f38cf9b1e92843f37307b24e7ff63ab8e53"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/libass/libass"
 PKG_URL="https://github.com/libass/libass/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
