388b2f13b969bf9614938f98b9f92cb1018afcb3
heitbaum <rudi@heitbaum.com>
2021-05-02 05:09:17 +0000

    RTL8188EU: update to 2021-05-02
    includes
    - docs: Fix typos in README.md

--- packages/linux-drivers/RTL8188EU/package.mk
+++ packages/linux-drivers/RTL8188EU/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="RTL8188EU"
-PKG_VERSION="b02c92bc92e22fd8b51c9acd6d602637cfce7256"
-PKG_SHA256="e855e210b27fc33212c558d34c32e0a5ccc32209d5c7e3f71fcf67695c224594"
+PKG_VERSION="b4d6cb1d6f0b8fbc978002faa9272853da3745d7"
+PKG_SHA256="f61015144fd65ebd292ff114a82cf7a0218edd11f8684f09db5ff7f270d4d1e8"
 PKG_LICENSE="GPL"
 # realtek: PKG_SITE="http://www.realtek.com.tw/downloads/downloadsView.aspx?Langid=1&PFid=48&Level=5&Conn=4&ProdID=274&DownTypeID=3&GetDown=false&Downloads=true"
 PKG_SITE="https://github.com/lwfinger/rtl8188eu"
