387a080dae8d241e2ed0b97618b2f8843661350e
heitbaum <rudi@heitbaum.com>
2021-05-05 10:39:55 +0000

    gmmlib: update to 21.1.3

--- packages/multimedia/gmmlib/package.mk
+++ packages/multimedia/gmmlib/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gmmlib"
-PKG_VERSION="21.1.2"
-PKG_SHA256="ecd3b39ae7fd8414e0a45b4b23877c5c13eec692b0a21e0126f336e9c02eea5d"
+PKG_VERSION="21.1.3"
+PKG_SHA256="c459a95243075296c72a40674c449ffcc314ed6d21ad0291c1603dbc11f4a74e"
 PKG_ARCH="x86_64"
 PKG_LICENSE="MIT"
 PKG_SITE="https://01.org/linuxmedia"
