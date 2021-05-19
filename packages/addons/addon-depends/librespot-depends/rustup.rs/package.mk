94ca1a467ba504bce5c4c2057a1b75f6ff9fe869
heitbaum <rudi@heitbaum.com>
2021-05-19 12:18:00 +0000

    rustup.rs: update to 1.24.2

--- packages/addons/addon-depends/librespot-depends/rustup.rs/package.mk
+++ packages/addons/addon-depends/librespot-depends/rustup.rs/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rustup.rs"
-PKG_VERSION="1.24.1"
-PKG_SHA256="e69bce5a4b1abe05489b19d2906c258b27f70ff8b13f59e5932527ae6b77c6a6"
+PKG_VERSION="1.24.2"
+PKG_SHA256="a55ae51b4ba4f5db56e855a13703945ece42227370621f4630dfaa4767126f57"
 PKG_LICENSE="MIT"
 PKG_SITE="https://www.rust-lang.org"
 PKG_URL="https://github.com/rust-lang-nursery/rustup.rs/archive/${PKG_VERSION}.tar.gz"
