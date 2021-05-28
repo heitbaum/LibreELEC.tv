9a6f9a5d442c0dc197475a2c35fe2436a69e8b74
heitbaum <rudi@heitbaum.com>
2021-05-28 09:14:42 +0000

    cmake: update to 3.20.3

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="cmake"
-PKG_VERSION="3.20.2"
-PKG_SHA256="aecf6ecb975179eb3bb6a4a50cae192d41e92b9372b02300f9e8f1d5f559544e"
+PKG_VERSION="3.20.3"
+PKG_SHA256="4d008ac3461e271fcfac26a05936f77fc7ab64402156fb371d41284851a651b8"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cmake.org/"
 PKG_URL="https://cmake.org/files/v$(get_pkg_version_maj_min)/cmake-${PKG_VERSION}.tar.gz"
