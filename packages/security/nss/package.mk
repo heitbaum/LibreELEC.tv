005156f32cff548f6f5a63f43e21d1a337003be0
heitbaum <rudi@heitbaum.com>
2021-05-29 11:49:51 +0000

    nss: update to 3.66

--- packages/security/nss/package.mk
+++ packages/security/nss/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nss"
-PKG_VERSION="3.65"
-PKG_SHA256="5da275c751d0a4f624089d4589ae68b0dfc9cb77bc846ef903fb819fbf678976"
+PKG_VERSION="3.66"
+PKG_SHA256="4eb72ca78b497a2a425139fdcfb9068cbd318dd51542baaa5365fcfbcb165009"
 PKG_LICENSE="Mozilla Public License"
 PKG_SITE="http://ftp.mozilla.org/"
 PKG_URL="https://ftp.mozilla.org/pub/security/nss/releases/NSS_${PKG_VERSION//./_}_RTM/src/nss-${PKG_VERSION}-with-nspr-$(get_pkg_version nspr).tar.gz"
