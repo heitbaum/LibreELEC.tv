7db737abefa234aa38e15687002ec3499a49d740
heitbaum <rudi@heitbaum.com>
2021-06-01 13:19:32 +0000

    mtools: update to 4.0.29

--- packages/tools/mtools/package.mk
+++ packages/tools/mtools/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mtools"
-PKG_VERSION="4.0.27"
-PKG_SHA256="9c88c8d9244e54768e40b92c005e61ec4e084ea7c070df9184caf1a6408838b3"
+PKG_VERSION="4.0.29"
+PKG_SHA256="c6b50b4603e7afe54ba0109094669373028db9af743f5c541bca4b7664aefec7"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/mtools/"
 PKG_URL="http://ftpmirror.gnu.org/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
