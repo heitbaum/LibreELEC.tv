4846114f8f4d0252a3810dbda77daace395b50e9
heitbaum <rudi@heitbaum.com>
2021-06-09 10:16:15 +0000

    Revert "iperf: update to 3.10.1"
    This reverts commit 249a5711ee3c1f2fef8d3dbdc5d00a7d89fb2cec.

--- packages/addons/addon-depends/network-tools-depends/iperf/package.mk
+++ packages/addons/addon-depends/network-tools-depends/iperf/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iperf"
-PKG_VERSION="3.10.1"
-PKG_SHA256="6a4bb4d5c124b3fa64dfbda469ab16857ad6565310bcaa3dd8cd32f96c2fc473"
+PKG_VERSION="3.10"
+PKG_SHA256="03cc873533ae6af54916f886a3e204c3bf1bd3e9de33759dc2718feade63fd4e"
 PKG_LICENSE="BSD"
 PKG_SITE="http://software.es.net/iperf/"
 PKG_URL="https://github.com/esnet/iperf/archive/${PKG_VERSION}.tar.gz"
