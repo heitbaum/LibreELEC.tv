616beab6be46cdb50c4953185280e1da25dc5f62
heitbaum <rudi@heitbaum.com>
2021-06-13 13:18:26 +0000

    tinyxml2: update to 9.0.0

--- packages/textproc/tinyxml2/package.mk
+++ packages/textproc/tinyxml2/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tinyxml2"
-PKG_VERSION="8.1.0"
-PKG_SHA256="23c95af3b981cf9a56c7f669505832c93427cb684811bcb6c9311bd18fa9bc30"
+PKG_VERSION="9.0.0"
+PKG_SHA256="cc2f1417c308b1f6acc54f88eb70771a0bf65f76282ce5c40e54cfe52952702c"
 PKG_LICENSE="zlib"
 PKG_SITE="http://www.grinninglizard.com/tinyxml2/index.html"
 PKG_URL="https://github.com/leethomason/tinyxml2/archive/${PKG_VERSION}.tar.gz"
