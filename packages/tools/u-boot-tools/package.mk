65551daf992b0e62ce2f8174997988eb972a2bad
heitbaum <rudi@heitbaum.com>
2021-06-13 13:11:36 +0000

    u-boot-tools: update to 2021.07-rc4

--- packages/tools/u-boot-tools/package.mk
+++ packages/tools/u-boot-tools/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="u-boot-tools"
-PKG_VERSION="2021.07-rc3"
+PKG_VERSION="2021.07-rc4"
 #PKG_SHA256="0d438b1bb5cceb57a18ea2de4a0d51f7be5b05b98717df05938636e0aadfe11a"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
