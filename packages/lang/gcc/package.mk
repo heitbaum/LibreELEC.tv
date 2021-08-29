4c41b7501444e0f62367698dea93dda4f4c642d7
heitbaum <rudi@heitbaum.com>
2021-08-29 16:32:58 +1000

    gcc: enable libgomp for HOST and TARGET

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -52,7 +52,6 @@ GCC_COMMON_CONFIGURE_OPTS="--target=${TARGET_NAME} \
                            --disable-libmudflap \
                            --disable-libitm \
                            --disable-libquadmath \
-                           --disable-libgomp \
                            --disable-libmpx \
                            --disable-libssp \
                            --enable-__cxa_atexit"
@@ -78,6 +77,8 @@ PKG_CONFIGURE_OPTS_HOST="${GCC_COMMON_CONFIGURE_OPTS} \
                          --enable-tls \
                          --enable-shared \
                          ${OPTS_STATIC} \
+                         --enable-c99 \
+                         --enable-libgomp \
                          --enable-long-long \
                          --enable-threads=posix \
                          --disable-libstdcxx-pch \
