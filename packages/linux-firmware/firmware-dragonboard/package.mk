5b81c609b300117125ef8021085afc232a9a9418
heitbaum <rudi@heitbaum.com>
2021-10-17 18:27:08 +1100

    firmware-dragonboard: WIP

--- packages/linux-firmware/firmware-dragonboard/package.mk
+++ packages/linux-firmware/firmware-dragonboard/package.mk
@@ -11,6 +11,13 @@ PKG_URL="https://developer.qualcomm.com/download/db410c/firmware-410c-${PKG_VERS
 PKG_DEPENDS_TARGET="toolchain mtools:host"
 PKG_LONGDESC="Additional firmware for Dragonboard 410c"
 PKG_TOOLCHAIN="manual"
+# https://developer.qualcomm.com/qfile/67733/dragonboard_410c.zip.1.0-r1036.1.zip
+# https://developer.qualcomm.com/qfile/35475/linux-board-support-package-r1034.2.1.zip
+# https://developer.qualcomm.com/qfile/34896/linux-board-support-package-r1032.1.1.zip
+# https://developer.qualcomm.com/qfile/34250/linux-board-support-package-r1032.1.zip
+# https://developer.qualcomm.com/qfile/33505/linux-bootloader-and-firmware-v1030.4-psci.zip
+# https://developer.qualcomm.com/qfile/33428/linux-board-support-package-v1.4.zip
+#
 
 unpack() {
   mkdir -p ${PKG_BUILD}
