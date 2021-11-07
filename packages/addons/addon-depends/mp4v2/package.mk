36b64412c4bad412fa73e5076da5254a1f37c81c
heitbaum <rudi@heitbaum.com>
2021-11-07 19:12:41 +1100

    mp4v2: initial package

---
+++ packages/addons/addon-depends/mp4v2/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="mp4v2"
+PKG_VERSION="5.0.1"
+PKG_SHA256="de31e430e2641f25b67d10c47b0cda35279881b0196120e33bcd71b9cef1bd58"
+PKG_LICENSE="MPL-1.1"
+PKG_SITE="https://code.google.com/archive/p/mp4v2/"
+PKG_URL="https://github.com/TechSmith/mp4v2/archive/refs/tags/Release-ThirdParty-MP4v2-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libjpeg-turbo"
+PKG_LONGDESC="MP4v2 Library: This library provides functions to read, create, and modify mp4 files"
+#PKG_TOOLCHAIN="autotools"
