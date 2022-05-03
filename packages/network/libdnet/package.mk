685b268405ca1614aa4a8829ca8d6c5ba8930098
Rudi Heitbaum <rudi@heitbaum.com>
2022-05-03 11:26:23 +0000

    libdnet: update to 1.16.1

--- packages/network/libdnet/package.mk
+++ packages/network/libdnet/package.mk
@@ -3,23 +3,24 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libdnet"
-PKG_VERSION="1.14"
-PKG_SHA256="592599c54a57102a177270f3a2caabda2c2ac7768b977d7458feba97da923dfe"
+PKG_VERSION="1.16.1"
+PKG_SHA256="b151c4913758ef80daf4558361b4f2680a37b01d59272625bb78c77183062c63"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/ofalk/libdnet"
 PKG_URL="https://github.com/ofalk/libdnet/archive/${PKG_NAME}-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain libcheck"
 PKG_LONGDESC="A simplified, portable interface to several low-level networking routines"
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="+pic"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_strlcat=no \
-                           ac_cv_func_strlcpy=no \
-                           --enable-static \
-                           --disable-shared \
-                           --disable-python"
-
 pre_configure_target() {
+  PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_strlcat=no \
+                             ac_cv_func_strlcpy=no \
+                             --enable-static \
+                             --disable-shared \
+                             --with-check=${PKG_CONFIG_SYSROOT_DIR}/usr \
+                             --disable-python"
+
   export CFLAGS+=" -I${PKG_BUILD}/include"
 }
 
