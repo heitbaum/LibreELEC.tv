8d5aaf4895b792ffceaf5e4efb207fcc1400e584
Rudi Heitbaum <rudi@heitbaum.com>
2022-05-03 12:18:33 +0000

    libcheck: initial package

---
+++ packages/devel/libcheck/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libcheck"
+PKG_VERSION="0.15.2"
+PKG_SHA256="a8de4e0bacfb4d76dd1c618ded263523b53b85d92a146d8835eb1a52932fa20a"
+PKG_LICENSE="LGPLv2.1"
+PKG_SITE="https://libcheck.github.io/check/"
+PKG_URL="https://github.com/libcheck/check/releases/download/${PKG_VERSION}/check-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Check is a unit testing framework for C."
