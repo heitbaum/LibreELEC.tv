b3fb005ac590b2ba7590ca6db946bd078d1128d6
Rudi Heitbaum <rudi@heitbaum.com>
2022-05-31 10:42:23 +0000

    mesa-demos: update to 8.5.0 and PKG_URL and meson

--- packages/addons/addon-depends/multimedia-tools-depends/mesa-demos/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/mesa-demos/package.mk
@@ -2,18 +2,15 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa-demos"
-PKG_VERSION="8.4.0"
-PKG_SHA256="01e99c94a0184e63e796728af89bfac559795fb2a0d6f506fa900455ca5fff7d"
+PKG_VERSION="8.5.0"
+PKG_SHA256="cea2df0a80f09a30f635c4eb1a672bf90c5ddee0b8e77f4d70041668ef71aac1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
-PKG_SITE="http://www.mesa3d.org/"
-PKG_URL="ftp://ftp.freedesktop.org/pub/mesa/demos/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_SITE="https://www.mesa3d.org/"
+PKG_URL="https://archive.mesa3d.org/demos/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libX11 mesa glu glew"
 PKG_LONGDESC="Mesa 3D demos - installed are the well known glxinfo and glxgears."
-PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="-sysroot"
-
-PKG_CONFIGURE_OPTS_TARGET="--without-glut"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/bin
