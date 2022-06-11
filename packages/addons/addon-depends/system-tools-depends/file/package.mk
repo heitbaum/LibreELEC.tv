8f8a55316ecced3e92319fae405fa311392d58f4
Rudi Heitbaum <rudi@heitbaum.com>
2022-06-11 01:51:34 +0000

    file: update to 5.42

--- packages/addons/addon-depends/system-tools-depends/file/package.mk
+++ packages/addons/addon-depends/system-tools-depends/file/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="file"
-PKG_VERSION="5.41"
-PKG_SHA256="088fd09e8f2f7e8fda05e07ba9586df8708e1676a44db207b4496bd44e7f49f4"
+PKG_VERSION="5.42"
+PKG_SHA256="d7374d06451154a628831df58e835fa3263825d0ad593df0fb8a911418d27863"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.darwinsys.com/file/"
 PKG_URL="https://github.com/file/file/archive/FILE${PKG_VERSION/./_}.tar.gz"
