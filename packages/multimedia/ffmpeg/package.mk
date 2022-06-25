95cc795bcd3b6156f1ea14924b501851bd0822f8
Christian Hewitt <christianshewitt@gmail.com>
2022-06-25 09:14:14 +0000

    ffmpeg: use JC sources for testing

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -9,7 +9,7 @@ PKG_DEPENDS_TARGET="toolchain zlib bzip2 openssl speex"
 PKG_LONGDESC="FFmpeg is a complete, cross-platform solution to record, convert and stream audio and video."
 
 case "${PROJECT}" in
-  Amlogic)
+  Amlogic|Samsung)
     PKG_VERSION="0e5290bcac015e52f6a65dafaf41ea125816257f"
     PKG_FFMPEG_BRANCH="dev/4.4/rpi_import_1"
     PKG_SHA256="4bd6e56920b90429bc09e43cda554f5bb9125c4ac090b4331fc459bb709eea68"
