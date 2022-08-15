f5c2f5573f047618e0f445ad74937db17faa3c52
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-15 08:44:58 +0000

    libnvme: initial package

---
+++ packages/tools/libnvme/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libnvme"
+PKG_VERSION="1.4"
+PKG_SHA256="19a982e6b38fd0de9c4ed72196d69606f5829f8fab3793393468e2dc27b3bb96"
+PKG_LICENSE="GPL"
+PKG_SITE="https://nvmexpress.org"
+PKG_URL="https://github.com/linux-nvme/libnvme/archive/refs/tags/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain json-c util-linux"
+PKG_LONGDESC="C Library for NVM Express on Linux"
+
+PKG_MESON_OPTS_TARGET="-Ddocs=false \
+                       -Ddocs-build=false \
+                       -Dpython=disabled \
+                       -Dopenssl=disabled \
+                       --default-library=static"
