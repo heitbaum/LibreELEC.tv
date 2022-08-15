cc510c35c1b0aff5495e01a9867546d10cd89e0e
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-15 08:44:37 +0000

    nvme-cli: initial package

---
+++ packages/tools/nvme-cli/package.mk
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="nvme-cli"
+PKG_VERSION="2.16"
+PKG_SHA256="989682ed7b250a2c7a8127e362ffc5d29f5c370127abe405be09c73216da2b97"
+PKG_LICENSE="GPL"
+PKG_SITE="https://nvmexpress.org"
+PKG_URL="https://github.com/linux-nvme/nvme-cli/archive/refs/tags/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain json-c libnvme util-linux zlib"
+PKG_LONGDESC="NVMe management command line interface"
+
+PKG_MESON_OPTS_TARGET="-Ddocs=false \
+                       -Ddocs-build=false \
+                       -Dexamples=false \
+                       -Dtests=false"
+
+post_makeinstall_target() {
+  safe_remove ${INSTALL}/etc
+  safe_remove ${INSTALL}/usr/lib
+}
