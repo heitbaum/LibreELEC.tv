4e3dae09864f28eb8461b8fb3019518ecd52f096
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-19 07:59:18 +0000

    mesa: update to 22.2.0-rc3

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="22.1.7"
-PKG_SHA256="da838eb2cf11d0e08d0e9944f6bd4d96987fdc59ea2856f8c70a31a82b355d89"
+PKG_VERSION="22.2.0-rc3"
+PKG_SHA256="db392307a02f97b7a8237809e28b627306356a2b27bb911bf9f4344b66313e39"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
