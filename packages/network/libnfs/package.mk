9a07c09de658c285ba42f397eaab9fac153f3618
Rudi Heitbaum <rudi@heitbaum.com>
2022-08-30 22:38:47 +0000

    Revert "libnfs: migrate to cmake build"
    This reverts commit d66a37d00c11e3a92cb67e43db85dd28416bb34b.

--- packages/network/libnfs/package.mk
+++ packages/network/libnfs/package.mk
@@ -10,11 +10,9 @@ PKG_SITE="https://github.com/sahlberg/libnfs"
 PKG_URL="https://github.com/sahlberg/libnfs/archive/libnfs-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="A client library for accessing NFS shares over a network."
+PKG_TOOLCHAIN="autotools"
 
-PKG_CMAKE_OPTS_TARGET="-DENABLE_DOCUMENTATION=OFF \
-                       -DENABLE_EXAMPLES=OFF \
-                       -DENABLE_TESTS=OFF \
-                       -DENABLE_UTILS=ON"
+PKG_CONFIGURE_OPTS_TARGET="--disable-examples"
 
 pre_configure_target() {
   export CFLAGS="${CFLAGS} -D_FILE_OFFSET_BITS=64"
