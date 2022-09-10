bd911a025466c3cbf69b3277477224542be5a323
Rudi Heitbaum <rudi@heitbaum.com>
2022-09-10 06:51:53 +0000

    fdupes: update to 2.2.1

--- packages/addons/addon-depends/system-tools-depends/fdupes/package.mk
+++ packages/addons/addon-depends/system-tools-depends/fdupes/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fdupes"
-PKG_VERSION="2.2.0"
-PKG_SHA256="174e488c21c3d0475228fe95c268c7f518d92f2732bdf36d1964dc285538e1ac"
+PKG_VERSION="2.2.1"
+PKG_SHA256="846bb79ca3f0157856aa93ed50b49217feb68e1b35226193b6bc578be0c5698d"
 PKG_LICENSE="GPL"
-PKG_SITE="http://premium.caribe.net/~adrian2/fdupes.html"
+PKG_SITE="https://github.com/adrianlopezroche/fdupes"
 PKG_URL="https://github.com/adrianlopezroche/fdupes/releases/download/v${PKG_VERSION}/fdupes-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_LONGDESC="A program for identifying or deleting duplicate files residing within specified directories."
