389d9ae4364de595389e16d31abc65814867ecc0
Rudi Heitbaum <rudi@heitbaum.com>
2022-09-12 10:19:06 +0000

    ccache: update to 4.6.3 and now builds with cmake
    - add cmake:host dependency
    - add CCACHE_COMPRESSION option
    - handle CCACHE_DISABLE
    - set compression level
      - to CCACHE_COMPRESSLEVEL during package build
      - default to 0

--- packages/devel/ccache/package.mk
+++ packages/devel/ccache/package.mk
@@ -3,37 +3,56 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="ccache"
-PKG_VERSION="3.7.12"
-PKG_SHA256="a02f4e8360dc6618bc494ca35b0ae21cea080f804a4898eab1ad3fcd108eb400"
+PKG_VERSION="4.6.3"
+PKG_SHA256="1e3a251bb112632553b8255a78661fe526c3a16598496d51128c32b218fd8b22"
 PKG_LICENSE="GPL"
 PKG_SITE="https://ccache.dev/download.html"
 PKG_URL="https://github.com/ccache/ccache/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_HOST="make:host zstd:host"
+PKG_DEPENDS_HOST="cmake:host make:host zstd:host"
 PKG_LONGDESC="A compiler cache to speed up re-compilation of C/C++ code by caching."
+# Override toolchain as ninja is not built yet
+PKG_TOOLCHAIN="cmake-make"
 
-PKG_CONFIGURE_OPTS_HOST="--with-bundled-zlib"
+configure_host() {
+  # custom cmake build to override the LOCAL_CC/CXX
+  setup_toolchain host:cmake-make
+  cp ${CMAKE_CONF} cmake-ccache.conf
 
-pre_configure_host() {
-  export CC=${LOCAL_CC}
-  export CXX=${LOCAL_CXX}
+  echo "SET(CMAKE_C_COMPILER   $LOCAL_CC)"  >> cmake-ccache.conf
+  echo "SET(CMAKE_CXX_COMPILER $LOCAL_CXX)" >> cmake-ccache.conf
+
+  cmake -DCMAKE_TOOLCHAIN_FILE=cmake-ccache.conf \
+        -DCMAKE_INSTALL_PREFIX=${TOOLCHAIN} \
+        -DZSTD_FROM_INTERNET=OFF \
+        -DREDIS_STORAGE_BACKEND=OFF \
+        ..
+}
+
+make_host() {
+  :
 }
 
 post_makeinstall_host() {
 # setup ccache
   if [ -z "${CCACHE_DISABLE}" ]; then
     ${TOOLCHAIN}/bin/ccache --max-size=${CCACHE_CACHE_SIZE}
+    ${TOOLCHAIN}/bin/ccache --set-config compression_level=${CCACHE_COMPRESSLEVEL}
+    ${TOOLCHAIN}/bin/ccache --set-config compression=${CCACHE_COMPRESSION}
+    ${TOOLCHAIN}/bin/ccache --set-config disable=false
+  else
+    ${TOOLCHAIN}/bin/ccache --set-config disable=true
   fi
 
   cat > ${TOOLCHAIN}/bin/host-gcc <<EOF
 #!/bin/sh
-${TOOLCHAIN}/bin/ccache ${CC} "\$@"
+${TOOLCHAIN}/bin/ccache ${LOCAL_CC} "\$@"
 EOF
 
   chmod +x ${TOOLCHAIN}/bin/host-gcc
 
   cat > ${TOOLCHAIN}/bin/host-g++ <<EOF
 #!/bin/sh
-${TOOLCHAIN}/bin/ccache ${CXX} "\$@"
+${TOOLCHAIN}/bin/ccache ${LOCAL_CXX} "\$@"
 EOF
 
   chmod +x ${TOOLCHAIN}/bin/host-g++
