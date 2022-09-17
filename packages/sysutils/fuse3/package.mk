286ff751f1e3f0f2eeef866d1b151359453bc57e
Rudi Heitbaum <rudi@heitbaum.com>
2022-09-17 05:05:36 +0000

    fuse3: add build dependency on systemd

--- packages/sysutils/fuse3/package.mk
+++ packages/sysutils/fuse3/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="33b8a92d6f7a88e6a889f0009206933482f48f3eb85d88cf09ef551313ac7373"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libfuse/libfuse/"
 PKG_URL="https://github.com/libfuse/libfuse/releases/download/fuse-${PKG_VERSION}/fuse-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain systemd"
 PKG_LONGDESC="FUSE is an interface for userspace programs to export a filesystem to the Linux kernel."
 
 PKG_MESON_OPTS_TARGET="-Ddisable-mtab=false \
