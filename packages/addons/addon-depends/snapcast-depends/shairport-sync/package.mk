52db6c18abbbfb08d7746ddc403efb1ba6c385af
Rudi Heitbaum <rudi@heitbaum.com>
2022-10-02 02:59:36 +0000

    shairport-sync: update to 4.1-rc0 with AirPlay 2

--- packages/addons/addon-depends/snapcast-depends/shairport-sync/package.mk
+++ packages/addons/addon-depends/snapcast-depends/shairport-sync/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="shairport-sync"
-PKG_VERSION="3.3.9"
-PKG_SHA256="17990cb2620551caa07a1c3b371889e55803071eaada04e958c356547a7e1795"
+PKG_VERSION="4.1-rc0"
+PKG_SHA256="133103d00a2f668ba803bf88ad192ce2b5bd900fc8e4bfbbcb9f164895f81a40"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/mikebrady/shairport-sync"
 PKG_URL="https://github.com/mikebrady/shairport-sync/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib avahi libconfig libdaemon libsndfile openssl popt pulseaudio soxr"
+PKG_DEPENDS_TARGET="toolchain alsa-lib avahi ffmpeg libconfig libdaemon libplist libsndfile libsodium nqptp openssl popt pulseaudio soxr util-linux"
 PKG_LONGDESC="AirPlay audio player."
 PKG_TOOLCHAIN="autotools"
 PKG_BUILD_FLAGS="-sysroot"
@@ -22,4 +22,6 @@ PKG_CONFIGURE_OPTS_TARGET="--with-alsa \
                            --with-soxr \
                            --with-ssl=openssl \
                            --with-stdout \
-                           --without-configfiles"
+                           --without-configfiles \
+                           --with-airplay-2"
+                           #--with-systemd
