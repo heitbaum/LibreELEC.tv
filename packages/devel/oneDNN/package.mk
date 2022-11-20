87c5092726f6a69b741ac76ea750ce965e3b0cea
Rudi Heitbaum <rudi@heitbaum.com>
2022-11-20 05:12:42 +0000

    oneDNN: update to 2.7.2

--- packages/devel/oneDNN/package.mk
+++ packages/devel/oneDNN/package.mk
@@ -1,9 +1,9 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: GPL-2.0-only
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="oneDNN"
-PKG_VERSION="2.5.2"
-PKG_SHA256="11d50235afa03571dc70bb6d96a98bfb5d9b53e8c00cc2bfbde78588bd01f6a3"
+PKG_VERSION="2.7.2"
+PKG_SHA256="49018ee24b1b13c166510354c03c2f1489262b10ee09b12cdeaec5745f99ac50"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/oneapi-src/oneDNN"
 PKG_URL="https://github.com/oneapi-src/oneDNN/archive/v${PKG_VERSION}.tar.gz"
