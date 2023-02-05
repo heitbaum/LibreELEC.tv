3f5caece09e90816304c9cff4feee57708211e45
Rudi Heitbaum <rudi@heitbaum.com>
2023-02-05 10:55:13 +0000

    conmon: initial package

---
+++ packages/addons/addon-depends/podman/conmon/package.mk
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="conmon"
+PKG_VERSION="2.1.7"
+PKG_SHA256="7d0f9a2f7cb8a76c51990128ac837aaf0cc89950b6ef9972e94417aa9cf901fe"
+PKG_LICENSE="Apache-2.0"
+PKG_SITE="https://github.com/containers/conmon"
+PKG_URL="https://github.com/containers/conmon/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain glib libseccomp systemd"
+PKG_LONGDESC="An OCI container runtime monitor"
+
+# Git commit of the matching release https://github.com/containers/conmon
+export PKG_GIT_COMMIT="f633919178f6c8ee4fb41b848a056ec33f8d707d"
+
+pre_configure_target() {
+  export PKG_CONFIG_PATH="$(get_install_dir libseccomp)/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
+  export GIT_COMMIT=${PKG_GIT_COMMIT}
+}
