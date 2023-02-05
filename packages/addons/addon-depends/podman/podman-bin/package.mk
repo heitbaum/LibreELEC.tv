bc5e6a728b1cc597492d37eda583def363b0042e
Rudi Heitbaum <rudi@heitbaum.com>
2023-02-05 09:36:17 +0000

    podman-bin: initial package

---
+++ packages/addons/addon-depends/podman/podman-bin/package.mk
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="podman-bin"
+PKG_VERSION="4.4.2"
+PKG_SHA256="59cec158438efa8a3e651b19e150d9afd90f7e3f07c30605a997e18b8c54b67c"
+PKG_LICENSE="Apache-2.0"
+PKG_SITE="https://podman.io/"
+PKG_URL="https://github.com/containers/podman/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain go:host gpgme libassuan libseccomp systemd"
+PKG_LONGDESC="Podman: A tool for managing OCI containers and pods."
+PKG_TOOLCHAIN="manual"
+
+# Git commit of the matching release https://github.com/containers/podman
+export PKG_GIT_COMMIT="34e8f3933242f2e566bbbbf343cf69b7d506c1cf"
+
+PKG_PODMAN_BUILDTAGS="exclude_graphdriver_devicemapper \
+                      exclude_graphdriver_btrfs \
+                      seccomp \
+                      systemd"
+
+configure_target() {
+  export PKG_CONFIG_PATH="$(get_install_dir libassuan)/usr/lib/pkgconfig:$(get_install_dir gpgme)/usr/lib/pkgconfig:$(get_install_dir libseccomp)/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
+
+  go_configure
+
+  # used for podman commit
+  export GIT_COMMIT=${PKG_GIT_COMMIT}
+}
+
+make_target() {
+  make BUILDTAGS="${PKG_PODMAN_BUILDTAGS}" \
+       PREFIX=/storage/.kodi/addons/service.system.podman \
+       HELPER_BINARIES_DIR=/storage/.kodi/addons/service.system.podman/bin \
+       EXTRA_LDFLAGS="-X github.com/containers/image/v5/sysregistries.systemRegistriesConfPath=/storage/.kodi/userdata/addon_data/service.system.podman/podman/etc/containers/registries.conf -X github.com/containers/image/v5/sysregistries.systemRegistriesConfDirectoryPath=/storage/.kodi/userdata/addon_data/service.system.podman/podman/etc/containers/registries.conf.d -X github.com/containers/image/v5/signature.systemDefaultPolicyPath=/storage/.kodi/addons/service.system.podman/etc/containers/policy.json -extldflags=-L$(get_install_dir gpgme)/usr/lib" \
+       binaries
+}
+
+makeinstall_target() {
+  :
+}
