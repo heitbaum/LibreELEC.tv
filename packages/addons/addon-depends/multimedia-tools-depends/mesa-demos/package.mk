7fb43bb8e197e2d90acd350b860701269102d04e
Rudi Heitbaum <rudi@heitbaum.com>
2023-03-23 12:44:09 +0000

    mesa-demos: update to 9.0.0

--- packages/addons/addon-depends/multimedia-tools-depends/mesa-demos/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/mesa-demos/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa-demos"
-PKG_VERSION="8.5.0"
-PKG_SHA256="cea2df0a80f09a30f635c4eb1a672bf90c5ddee0b8e77f4d70041668ef71aac1"
+PKG_VERSION="9.0.0"
+PKG_SHA256="3046a3d26a7b051af7ebdd257a5f23bfeb160cad6ed952329cdff1e9f1ed496b"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.mesa3d.org/"
-PKG_URL="https://archive.mesa3d.org/demos/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://archive.mesa3d.org/demos/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libX11 mesa glu glew"
 PKG_LONGDESC="Mesa 3D demos - installed are the well known glxinfo and glxgears."
 PKG_BUILD_FLAGS="-sysroot"
