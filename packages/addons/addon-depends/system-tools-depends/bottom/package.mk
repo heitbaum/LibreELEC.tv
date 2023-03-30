0a184ed489d074876d19748fdf79e7e4876521f0
Rudi Heitbaum <rudi@heitbaum.com>
2023-03-30 11:08:40 +0000

    bottom: master

--- packages/addons/addon-depends/system-tools-depends/bottom/package.mk
+++ packages/addons/addon-depends/system-tools-depends/bottom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bottom"
-PKG_VERSION="0.8.0"
-PKG_SHA256="0fe6a826d18570ab33b2af3b26ce28c61e3aa830abb2b622f2c3b81da802437a"
+PKG_VERSION="e36854641cdf7d46907c579906f2676b604fddc4"
+PKG_SHA256="e3f8ea0b0fdb6477e3ec3e6326d50ccf8f84234e424f44d2f570a79b7209dfc4"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/ClementTsang/bottom"
 PKG_URL="https://github.com/ClementTsang/bottom/archive/${PKG_VERSION}.tar.gz"
