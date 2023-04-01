d87add5eb9df65412944e429d6475686e910f59c
Rudi Heitbaum <rudi@heitbaum.com>
2023-04-01 04:34:37 +0000

    oneDNN: update to 3.1

--- packages/devel/oneDNN/package.mk
+++ packages/devel/oneDNN/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="oneDNN"
-PKG_VERSION="2.7.2"
-PKG_SHA256="49018ee24b1b13c166510354c03c2f1489262b10ee09b12cdeaec5745f99ac50"
+PKG_VERSION="3.1"
+PKG_SHA256="28e31f2d576e1a7e3a796f5c33c1d733c256078cff1c48b9e2a692d5975e1401"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/oneapi-src/oneDNN"
 PKG_URL="https://github.com/oneapi-src/oneDNN/archive/v${PKG_VERSION}.tar.gz"
