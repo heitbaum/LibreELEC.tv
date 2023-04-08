82614aae407bdf84924c1ed166e5f91b48087a4e
Rudi Heitbaum <rudi@heitbaum.com>
2023-04-08 09:09:31 +0000

    libdnet: update to 1.16.4

--- packages/network/libdnet/package.mk
+++ packages/network/libdnet/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libdnet"
-PKG_VERSION="1.16.3"
-PKG_SHA256="83171a9f6e96d7a5047d6537fce4c376bdf6d867f8d49cf6ba434a0f3f7b45c1"
+PKG_VERSION="1.16.4"
+PKG_SHA256="7df1f0a3db9cf03b48cf50ab273fd6a20c1be99eb9344b9663fe3fd9ed9dab65"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/ofalk/libdnet"
 PKG_URL="https://github.com/ofalk/libdnet/archive/${PKG_NAME}-${PKG_VERSION}.tar.gz"
