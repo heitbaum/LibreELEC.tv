9afbcd149cc1771fec25551f72648e9e12f900fb
Rudi Heitbaum <rudi@heitbaum.com>
2023-04-13 12:55:48 +0000

    WIP inputstream.adaptive: yyyyy

--- packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="inputstream.adaptive"
-PKG_VERSION="21.1.0-Omega"
-PKG_SHA256="1e250d1ae55c0c9d9498ccb97212c55738388b1858cc4db997c05e32ee6b151d"
-PKG_REV="1"
+PKG_VERSION="65dbb6f0ac9fbb84f210791ca8b7f1704f4af0d6"
+PKG_SHA256="71babf2edbf3a4fe40412d8c483dc2fd580d3934c69fa6a74e8859b0cdff25a2"
+PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/xbmc/inputstream.adaptive"
 PKG_URL="https://github.com/xbmc/inputstream.adaptive/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain kodi-platform bento4 expat nss"
+PKG_DEPENDS_TARGET="toolchain kodi-platform bento4 expat nss pugixml"
 PKG_SECTION=""
 PKG_SHORTDESC="inputstream.adaptive"
 PKG_LONGDESC="inputstream.adaptive"
