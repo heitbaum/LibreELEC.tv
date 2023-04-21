fd3e00a4bed51d03f5cc12ada631191734df7686
Rudi Heitbaum <rudi@heitbaum.com>
2023-04-21 09:22:10 +0000

    util-linux: update to 2.39-rc3

--- packages/sysutils/util-linux/package.mk
+++ packages/sysutils/util-linux/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="util-linux"
-PKG_VERSION="2.39-rc2"
-PKG_SHA256="68b9d1965108172ff687db728a040747c0ed86f6b88b20d96b67af1622d46807"
+PKG_VERSION="2.39-rc3"
+PKG_SHA256="047e201f73561c40ec7b8689c136f166870e1f70036d4a1e33190461f4e7eee7"
 PKG_LICENSE="GPL"
 PKG_URL="https://www.kernel.org/pub/linux/utils/util-linux/v$(get_pkg_version_maj_min | cut -f 1 -d -)/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_HOST="ccache:host autoconf:host automake:host intltool:host libtool:host pkg-config:host"
