ee2c9a3a64ed66024b935b01f2c8b448688b48b8
Rudi Heitbaum <rudi@heitbaum.com>
2023-04-28 14:45:23 +0000

    libusbmuxd: update to new

--- packages/addons/addon-depends/libusbmuxd/package.mk
+++ packages/addons/addon-depends/libusbmuxd/package.mk
@@ -2,13 +2,15 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libusbmuxd"
-PKG_VERSION="2.0.2"
-PKG_SHA256="cc6a808553da4efa9fa5638be256d5ae020498795d9d260d280b87074e799b20"
+PKG_VERSION="3.0.2"
+PKG_SHA256="5da7b60d0fd78f8e1c0f68b7fceafde373a40a2791af20cdcce639f1174abad2"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.libimobiledevice.org"
 PKG_URL="https://github.com/libimobiledevice/libusbmuxd/releases/download/${PKG_VERSION}/libusbmuxd-${PKG_VERSION}.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain libplist"
+PKG_URL="https://github.com/libimobiledevice/libusbmuxd/archive/refs/heads/master.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libimobiledevice-glue libplist"
 PKG_LONGDESC="A USB multiplex daemon."
+PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
                            ac_cv_func_realloc_0_nonnull=yes \
