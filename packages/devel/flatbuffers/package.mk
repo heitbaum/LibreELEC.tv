cdb4be48ba3d9a1f3eec13638dda22ba3a9f6d11
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-09 15:31:18 +0000

    flatbuffers: update to 23.5.8

--- packages/devel/flatbuffers/package.mk
+++ packages/devel/flatbuffers/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="flatbuffers"
-PKG_VERSION="23.3.3"
-PKG_SHA256="8aff985da30aaab37edf8e5b02fda33ed4cbdd962699a8e2af98fdef306f4e4d"
+PKG_VERSION="23.5.8"
+PKG_SHA256="55b75dfa5b6f6173e4abf9c35284a10482ba65db886b39db511eba6c244f1e88"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/google/flatbuffers"
 PKG_URL="https://github.com/google/flatbuffers/archive/v${PKG_VERSION}.tar.gz"
