cae05fb89179966ca25702840ac5591a42f2d25e
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-11 09:50:00 +0000

    libfmt: update to 10.0.0

--- packages/devel/libfmt/package.mk
+++ packages/devel/libfmt/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libfmt"
-PKG_VERSION="9.1.0"
-PKG_SHA256="5dea48d1fcddc3ec571ce2058e13910a0d4a6bab4cc09a809d8b1dd1c88ae6f2"
+PKG_VERSION="10.0.0"
+PKG_SHA256="ede1b6b42188163a3f2e0f25ad5c0637eca564bd8df74d02e31a311dd6b37ad8"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/fmtlib/fmt"
 PKG_URL="https://github.com/fmtlib/fmt/archive/${PKG_VERSION}.tar.gz"
