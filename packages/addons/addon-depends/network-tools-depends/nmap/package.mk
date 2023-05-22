cf68d708eb0e04fa83ac3844fc045fb90740be66
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-22 14:16:24 +0000

    nmap: --with-zenmap=no

--- packages/addons/addon-depends/network-tools-depends/nmap/package.mk
+++ packages/addons/addon-depends/network-tools-depends/nmap/package.mk
@@ -18,7 +18,8 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --with-libdnet=included \
                            --with-liblua=included \
                            --with-liblinear=included \
-                           --with-openssl=${SYSROOT_PREFIX}"
+                           --with-openssl=${SYSROOT_PREFIX} \
+                           --with-zenmap=no"
 
 pre_configure_target() {
 # nmap fails to build in subdirs
