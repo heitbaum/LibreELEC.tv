a1b04a4c07606a8b4e668608b02cc2121482b975
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-28 13:37:14 +0000

    tpm2-tss: initial package

---
+++ packages/security/tpm2-tss/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="tpm2-tss"
+PKG_VERSION="4.0.1"
+PKG_SHA256="532a70133910b6bd842289915b3f9423c0205c0ea009d65294ca18a74087c950"
+PKG_LICENSE="BSD-3-Clause"
+PKG_SITE="https://tpm2-software.github.io"
+PKG_URL="https://github.com/tpm2-software/tpm2-tss/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain json-c"
+PKG_LONGDESC="TCG TPM2 Software Stack (TSS2)"
