bebeb7f5254bd1ac78c4032b8872289c9cea0596
Rudi Heitbaum <rudi@heitbaum.com>
2023-05-28 13:24:41 +0000

    tpm2-tools: initial package

---
+++ packages/security/tpm2-tools/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="tpm2-tools"
+PKG_VERSION="5.5"
+PKG_SHA256="1fdb49c730537bfdaed088884881a61e3bfd121e957ec0bdceeec0261236c123"
+PKG_LICENSE="BSD-3-Clause"
+PKG_SITE="https://tpm2-software.github.io"
+PKG_URL="https://github.com/tpm2-software/tpm2-tools/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain tpm2-tss"
+PKG_LONGDESC="Trusted Platform Module (TPM2.0) tools"
