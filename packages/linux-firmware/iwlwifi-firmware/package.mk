cda128dd722d1b3fea482132b82a667fae7110e7
Rudi Heitbaum <rudi@heitbaum.com>
2023-07-12 14:09:13 +0000

    iwlwifi: temp heitbaum 6.5

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="03bf32cb9e3d6545ceaf00be20caee89bae6b936"
-PKG_SHA256="81219e6618912b9591d3e7e7ddb42fa55786d9e1fbf881f8f1f5205c967973be"
+PKG_VERSION="4d40f266404f21f06f260b34c525817225c8f689"
+PKG_SHA256="5c79e2dc199cbcd6b4da4d8bc726ad0cbb636879fdb2036d1935427e1ad04a96"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
-PKG_URL="https://github.com/LibreELEC/iwlwifi-firmware/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/heitbaum/iwlwifi-firmware/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="iwlwifi-firmware: firmwares for various Intel WLAN drivers"
 PKG_TOOLCHAIN="manual"
