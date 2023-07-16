ad9c414b3a7977a0db435b7f1417a90478805f05
Rudi Heitbaum <rudi@heitbaum.com>
2023-07-16 13:44:51 +0000

    Revert "libimobiledevice: update to eda2c5ea71829f11d69342e6858d09aa53943938"
    This reverts commit baf9c8642e0edd1158de0e640b5ccd3b0e45a045.

--- packages/addons/addon-depends/libimobiledevice/package.mk
+++ packages/addons/addon-depends/libimobiledevice/package.mk
@@ -2,13 +2,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libimobiledevice"
-PKG_VERSION="eda2c5ea71829f11d69342e6858d09aa53943938"
-PKG_SHA256="1ec68277ee98e9e0694202479b9495f2e74689370efb36535fe57b3f17c6b3f8"
+PKG_VERSION="1.3.0"
+PKG_SHA256="53f2640c6365cd9f302a6248f531822dc94a6cced3f17128d4479a77bd75b0f6"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.libimobiledevice.org"
 PKG_URL="https://github.com/libimobiledevice/libimobiledevice/releases/download/${PKG_VERSION}/libimobiledevice-${PKG_VERSION}.tar.bz2"
-PKG_URL="https://github.com/libimobiledevice/libimobiledevice/archive/${PKG_VERSION}/libimobiledevice-${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain libimobiledevice-glue libplist libusbmuxd openssl"
+PKG_DEPENDS_TARGET="toolchain libusbmuxd openssl"
 PKG_LONGDESC="A cross-platform software library that talks the protocols to support Apple devices."
 PKG_TOOLCHAIN="autotools"
 
@@ -19,4 +18,9 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-static \
 
 post_configure_target() {
   libtool_remove_rpath libtool
+}
+
+post_makeinstall_target() {
+  mkdir -p "${SYSROOT_PREFIX}/usr/include/lib/libimobiledevice"
+    cp ${PKG_BUILD}/common/utils.h "${SYSROOT_PREFIX}/usr/include/libimobiledevice"
 }
