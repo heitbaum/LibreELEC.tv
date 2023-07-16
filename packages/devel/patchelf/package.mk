7d9700207ab2f9113307b93ef9137b1ff6d26601
Rudi Heitbaum <rudi@heitbaum.com>
2023-07-16 11:00:19 +0000

    patchelf: update to 0.18.0

--- packages/devel/patchelf/package.mk
+++ packages/devel/patchelf/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="patchelf"
-PKG_VERSION="0.17.2"
-PKG_SHA256="bae2ea376072e422c196218dd9bdef0548ccc08da4de9f36b4672df84ea2d8e2"
+PKG_VERSION="0.18.0"
+PKG_SHA256="1952b2a782ba576279c211ee942e341748fdb44997f704dd53def46cd055470b"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/NixOS/patchelf"
 PKG_URL="https://github.com/NixOS/patchelf/releases/download/${PKG_VERSION}/patchelf-${PKG_VERSION}.tar.bz2"
