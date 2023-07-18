668a3b72fc1e5df1084f32129c29a4469781163c
Rudi Heitbaum <rudi@heitbaum.com>
2023-07-18 08:38:38 +0000

    pv: update to 1.7.0

--- packages/addons/addon-depends/system-tools-depends/pv/package.mk
+++ packages/addons/addon-depends/system-tools-depends/pv/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pv"
-PKG_VERSION="1.6.20"
-PKG_SHA256="e831951eff0718fba9b1ef286128773b9d0e723e1fbfae88d5a3188814fdc603"
+PKG_VERSION="1.7.0"
+PKG_SHA256="1372b41053881a05e2df10cb054304decc0233261c0aa0e96185842fa5a422ad"
 PKG_LICENSE="GNU"
 PKG_SITE="http://www.ivarch.com/programs/pv.shtml"
 PKG_URL="http://www.ivarch.com/programs/sources/pv-${PKG_VERSION}.tar.bz2"
