b90fd9e7ab93e2389e9ce352729032244ecf677a
Rudi Heitbaum <rudi@heitbaum.com>
2023-09-17 05:21:15 +0000

    shairport-sync: update to 4.3.2

--- packages/addons/addon-depends/snapcast-depends/shairport-sync/package.mk
+++ packages/addons/addon-depends/snapcast-depends/shairport-sync/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="shairport-sync"
-PKG_VERSION="4.2"
-PKG_SHA256="649d95eede8b9284b2e8b9c97d18c1c64cffae0a6c75bc4f03e3ae494a3e25b6"
+PKG_VERSION="4.3.2"
+PKG_SHA256="dfb485c0603398032a00e51f84b874749bbf155b257adda3d270d5989de08bfd"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/mikebrady/shairport-sync"
 PKG_URL="https://github.com/mikebrady/shairport-sync/archive/${PKG_VERSION}.tar.gz"
