28ed09f8744f663183e0b7d0800ff552256a11c5
Rudi Heitbaum <rudi@heitbaum.com>
2023-11-08 23:05:08 +0000

    mesa: update to 23.3.0-rc3

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="23.3.0-rc2"
-PKG_SHA256="82253a37c7b0f1a7ff15fdd7a51694d228e1538e1e1747f52b56c5dfc8bc2957"
+PKG_VERSION="23.3.0-rc3"
+PKG_SHA256="99c9168fa247151cbc7de421504501d812ed450a309aede43023aa69def6cf44"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
