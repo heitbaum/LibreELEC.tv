c30c1cd3e0bf3ef757bb4a79bf895bfc3ee9406a
Rudi Heitbaum <rudi@heitbaum.com>
2023-11-18 12:03:22 +0000

    podman: enable lib.private via RPATH

--- packages/addons/service/podman/package.mk
+++ packages/addons/service/podman/package.mk
@@ -16,22 +16,23 @@ PKG_ADDON_NAME="Podman"
 PKG_ADDON_TYPE="xbmc.service"
 
 addon() {
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/{bin,lib}
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/{bin,lib.private}
 
     # conmon
     cp -P $(get_install_dir conmon)/usr/lib/podman/conmon ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
 
     # gpgme
-    cp -L $(get_install_dir gpgme)/usr/lib/libgpgme.so.11 ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
+    cp -L $(get_install_dir gpgme)/usr/lib/libgpgme.so.11 ${ADDON_BUILD}/${PKG_ADDON_ID}/lib.private
 
     # libseccomp
-    cp -L $(get_install_dir libseccomp)/usr/lib/libseccomp.so.2 ${ADDON_BUILD}/${PKG_ADDON_ID}/lib
+    cp -L $(get_install_dir libseccomp)/usr/lib/libseccomp.so.2 ${ADDON_BUILD}/${PKG_ADDON_ID}/lib.private
 
     # netavark
     cp -P $(get_install_dir netavark)/usr/local/libexec/podman/netavark ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
 
     # podman
     cp -P $(get_build_dir podman-bin)/bin/podman ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
+    patchelf --add-rpath '$ORIGIN/../lib.private' ${ADDON_BUILD}/${PKG_ADDON_ID}/bin/podman
     cp -P $(get_build_dir podman-bin)/bin/podman-remote ${ADDON_BUILD}/${PKG_ADDON_ID}/bin/podman-remote
 
     # runc
