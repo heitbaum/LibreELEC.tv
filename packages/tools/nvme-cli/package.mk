973ad55d8775a13e8fe51be75416fc80e6db88d9
Rudi Heitbaum <rudi@heitbaum.com>
2023-12-25 01:31:25 +0000

    nvme-cli: update to 2.7.1

--- packages/tools/nvme-cli/package.mk
+++ packages/tools/nvme-cli/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nvme-cli"
-PKG_VERSION="2.6"
-PKG_SHA256="7e2f11eb7a9c1b9343d537a32ae5c78f51de20cd4a6cdddb2bc2459c259b33d6"
+PKG_VERSION="2.7.1"
+PKG_SHA256="4c69d0f3c8b553110d0f63f5876d56627abd2d9f5d3904e2480cedd03cb15654"
 PKG_LICENSE="GPL"
 PKG_SITE="https://nvmexpress.org"
 PKG_URL="https://github.com/linux-nvme/nvme-cli/archive/refs/tags/v${PKG_VERSION}.tar.gz"
