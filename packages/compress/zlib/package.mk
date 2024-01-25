acd7e5c3669036d4bb1388f00f4853c3d22cca3c
Rudi Heitbaum <rudi@heitbaum.com>
2024-01-25 13:07:20 +0000

    Revert "zlib: migrate zlib 1.2.13 to zlib-ng 2.0.5"
    This reverts commit 429505acc08b5ec3ac5d2d3ec957b009af2883a0.

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -1,18 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="zlib"
-PKG_VERSION="2.0.5"
-PKG_SHA256="eca3fe72aea7036c31d00ca120493923c4d5b99fe02e6d3322f7c88dbdcd0085"
+PKG_VERSION="1.3.1"
+PKG_SHA256="9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23"
 PKG_LICENSE="OSS"
-PKG_SITE="https://github.com/zlib-ng/zlib-ng"
-PKG_URL="https://github.com/zlib-ng/zlib-ng/archive/refs/tags/${PKG_VERSION}.tar.gz"
+PKG_SITE="http://www.zlib.net"
+PKG_URL="https://zlib.net/fossils/${PKG_NAME}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="ccache:host cmake:host"
 PKG_DEPENDS_TARGET="cmake:host gcc:host"
-PKG_LONGDESC="zlib data compression library for the next generation systems"
+PKG_LONGDESC="A general purpose (ZIP) data compression library."
 PKG_TOOLCHAIN="cmake-make"
 
-PKG_CMAKE_OPTS_HOST="-DZLIB_COMPAT=ON"
+PKG_CMAKE_OPTS_HOST="-DINSTALL_PKGCONFIG_DIR=${TOOLCHAIN}/lib/pkgconfig"
 
-PKG_CMAKE_OPTS_TARGET="-DZLIB_COMPAT=ON"
+PKG_CMAKE_OPTS_TARGET="-DINSTALL_PKGCONFIG_DIR=/usr/lib/pkgconfig"
