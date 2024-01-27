d8806e156671a2274167e262b24ef09b66cc7127
Rudi Heitbaum <rudi@heitbaum.com>
2024-01-27 10:27:09 +0000

    Revert "simplejson-test"
    This reverts commit 96145aa5a03f2fc445a9075522d47684a10d0110.

--- packages/python/system/simplejson/package.mk
+++ packages/python/system/simplejson/package.mk
@@ -28,6 +28,5 @@ makeinstall_target() {
 post_makeinstall_target() {
   python_remove_source
 
-  #rm -rf ${INSTALL}/usr/lib/python*/site-packages/*/tests
-  touch ${INSTALL}/usr/lib/python3.11/site-packages/index.rst
+  rm -rf ${INSTALL}/usr/lib/python*/site-packages/*/tests
 }
