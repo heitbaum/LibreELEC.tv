461ff115ac205cbcc06f371d28cda3031f87bf26
Rudi Heitbaum <rudi@heitbaum.com>
2024-02-28 10:04:10 +0000

    Revert "libcap: compile and install progs"
    This reverts commit 0a6e70ebdf72ed28fcf5d0ade73ac9a4efed0d59.

--- packages/devel/libcap/package.mk
+++ packages/devel/libcap/package.mk
@@ -33,17 +33,6 @@ make_host() {
        lib=/lib \
        USE_GPERF=no \
        -C libcap libcap.pc libcap.a
-
-  make CC=${CC} \
-       AR=${AR} \
-       RANLIB=${RANLIB} \
-       CFLAGS="${HOST_CFLAGS}" \
-       BUILD_CFLAGS="${HOST_CFLAGS} -I${PKG_BUILD}/libcap/include" \
-       PAM_CAP=no \
-       lib=/lib \
-       USE_GPERF=no \
-       DYNAMIC=no \
-       -C progs
 }
 
 make_target() {
@@ -59,18 +48,6 @@ make_target() {
        lib=/lib \
        USE_GPERF=no \
        -C libcap libcap.pc libcap.a
-
-  make CC=${CC} \
-       AR=${AR} \
-       RANLIB=${RANLIB} \
-       CFLAGS="${TARGET_CFLAGS}" \
-       BUILD_CC=${HOST_CC} \
-       BUILD_CFLAGS="${HOST_CFLAGS} -I${PKG_BUILD}/libcap/include" \
-       PAM_CAP=no \
-       lib=/lib \
-       USE_GPERF=no \
-       DYNAMIC=no \
-       -C progs
 }
 
 makeinstall_host() {
@@ -82,9 +59,6 @@ makeinstall_host() {
 
   mkdir -p ${TOOLCHAIN}/include/sys
     cp libcap/include/sys/capability.h ${TOOLCHAIN}/include/sys
-
-  mkdir -p ${TOOLCHAIN}/sbin
-    cp progs/{capsh,getpcaps,getcap,setcap} ${TOOLCHAIN}/sbin
 }
 
 makeinstall_target() {
@@ -96,7 +70,4 @@ makeinstall_target() {
 
   mkdir -p ${SYSROOT_PREFIX}/usr/include/sys
     cp libcap/include/sys/capability.h ${SYSROOT_PREFIX}/usr/include/sys
-
-  mkdir -p ${INSTALL}/usr/sbin
-    cp progs/{capsh,getpcaps,getcap,setcap} ${INSTALL}/usr/sbin
 }
