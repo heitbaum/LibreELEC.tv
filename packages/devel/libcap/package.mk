d697f80e60de0a0af91c0092120e513024a7ea28
Rudi Heitbaum <rudi@heitbaum.com>
2024-02-28 10:04:08 +0000

    Revert "cap"
    This reverts commit 3bdb514c6878f305e4c7ef67cf216684babe3758.

--- packages/devel/libcap/package.mk
+++ packages/devel/libcap/package.mk
@@ -32,6 +32,17 @@ make_host() {
        lib=/lib \
        USE_GPERF=no \
        -C libcap libcap.pc libcap.a
+
+  make CC=${CC} \
+       AR=${AR} \
+       RANLIB=${RANLIB} \
+       CFLAGS="${HOST_CFLAGS}" \
+       BUILD_CFLAGS="${HOST_CFLAGS} -I${PKG_BUILD}/libcap/include" \
+       PAM_CAP=no \
+       lib=/lib \
+       USE_GPERF=no \
+       DYNAMIC=no \
+       -C progs
 }
 
 make_target() {
@@ -69,6 +80,9 @@ makeinstall_host() {
 
   mkdir -p ${TOOLCHAIN}/include/sys
     cp libcap/include/sys/capability.h ${TOOLCHAIN}/include/sys
+
+  mkdir -p ${TOOLCHAIN}/sbin
+    cp progs/{capsh,getpcaps,getcap,setcap} ${TOOLCHAIN}/sbin
 }
 
 makeinstall_target() {
@@ -82,5 +96,5 @@ makeinstall_target() {
     cp libcap/include/sys/capability.h ${SYSROOT_PREFIX}/usr/include/sys
 
   mkdir -p ${INSTALL}/usr/sbin
-    cp progs/{getcap,setcap} ${INSTALL}/usr/sbin
+    cp progs/{capsh,getpcaps,getcap,setcap} ${INSTALL}/usr/sbin
 }
