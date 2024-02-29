7565b9d8d4ca83537b4f397c73a731f4ab8a6b48
Rudi Heitbaum <rudi@heitbaum.com>
2024-02-29 21:15:02 +0000

    opus: update to githash a8e4ebb and meson

--- packages/audio/opus/package.mk
+++ packages/audio/opus/package.mk
@@ -2,22 +2,30 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="opus"
-PKG_VERSION="1.4"
-PKG_SHA256="c9b32b4253be5ae63d1ff16eea06b94b5f0f2951b7a02aceef58e3a3ce49c51f"
+PKG_VERSION="a8e4ebb550dbdbb6149ad02d390c34442c8db951"
+PKG_SHA256="4da441987f0885883b5605b5ab6ac9f92a7f381e1a48d8d87135c353ef6047f0"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.opus-codec.org"
 PKG_URL="https://github.com/xiph/opus/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/xiph/opus/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Codec designed for interactive speech and audio transmission over the Internet."
-PKG_TOOLCHAIN="configure"
 PKG_BUILD_FLAGS="+pic"
 
+post_unpack() {
+  # if using a git hash as a package version - set package_version
+  echo "PACKAGE_VERSION=\"1.4-git-${PKG_VERSION:0:7}\"" > ${PKG_BUILD}/package_version
+  (cd ${PKG_BUILD}; ./autogen.sh)
+}
+
 if [ "${TARGET_ARCH}" = "arm" ]; then
-  PKG_FIXED_POINT="--enable-fixed-point"
+  PKG_FIXED_POINT="-Dfixed-point=true"
 else
-  PKG_FIXED_POINT="--disable-fixed-point"
+  PKG_FIXED_POINT="-Dfixed-point=false"
 fi
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static \
-                           --disable-shared \
-                           ${PKG_FIXED_POINT}"
+PKG_MESON_OPTS_TARGET="-Ddefault_library=static \
+                       -Ddocs=disabled \
+                       -Dextra-programs=disabled \
+                       -Dtests=disabled \
+                       ${PKG_FIXED_POINT}"
