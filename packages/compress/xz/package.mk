08f7e38e8f9ae7ec901569ad6804e07be8a066e4
Rudi Heitbaum <rudi@heitbaum.com>
2024-03-30 03:42:02 +0000

    xz: drop host build configuration

--- packages/compress/xz/package.mk
+++ packages/compress/xz/package.mk
@@ -8,21 +8,10 @@ PKG_SHA256="829ccfe79d769748f7557e7a4429a64d06858e27e1e362e25d01ab7b931d9c95"
 PKG_LICENSE="GPL"
 PKG_SITE="https://tukaani.org/xz/"
 PKG_URL="https://github.com/tukaani-project/xz/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_HOST="ccache:host"
 PKG_DEPENDS_TARGET="autotools:host gcc:host"
 PKG_LONGDESC="A free general-purpose data compression software with high compression ratio."
-PKG_BUILD_FLAGS="+pic +pic:host"
+PKG_BUILD_FLAGS="+pic"
 PKG_TOOLCHAIN="configure"
-
-# never build shared or k0p happens when building
-# on fedora due to host selinux/liblzma
-PKG_CONFIGURE_OPTS_HOST="--disable-shared --enable-static \
-                         --disable-lzmadec \
-                         --disable-lzmainfo \
-                         --enable-lzma-links \
-                         --disable-nls \
-                         --disable-scripts \
-                         --enable-symbol-versions=no"
 
 PKG_CONFIGURE_OPTS_TARGET="--enable-static \
                            --disable-shared \
