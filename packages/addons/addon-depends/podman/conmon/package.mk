19cb68c1565e66c1098a10716732db7a00b85cb3
Rudi Heitbaum <rudi@heitbaum.com>
2024-05-01 03:40:59 +0000

    conmon: update to 2.1.12

--- packages/addons/addon-depends/podman/conmon/package.mk
+++ packages/addons/addon-depends/podman/conmon/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="conmon"
-PKG_VERSION="2.1.10"
-PKG_SHA256="455fabcbd4a5a5dc5e05374a71b62dc0b08ee865c2ba398e9dc9acac1ea1836a"
+PKG_VERSION="2.1.12"
+PKG_SHA256="842f0b5614281f7e35eec2a4e35f9f7b9834819aa58ecdad8d0ff6a84f6796a6"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/containers/conmon"
 PKG_URL="https://github.com/containers/conmon/archive/v${PKG_VERSION}.tar.gz"
