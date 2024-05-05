b15848644ef11630055766d9fee7379a85681eea
Rudi Heitbaum <rudi@heitbaum.com>
2024-05-05 01:03:21 +0000

    libnvme: update to 1.9

--- packages/tools/libnvme/package.mk
+++ packages/tools/libnvme/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnvme"
-PKG_VERSION="1.8"
-PKG_SHA256="d59939a280eec41eb7a716e1681d0d0c612099385204ffb55d07134a6be08d75"
+PKG_VERSION="1.9"
+PKG_SHA256="455867060d2b7563eab59fe21779dff469d98465028997178c7efbe4b8763206"
 PKG_LICENSE="GPL"
 PKG_SITE="https://nvmexpress.org"
 PKG_URL="https://github.com/linux-nvme/libnvme/archive/refs/tags/v${PKG_VERSION}.tar.gz"
