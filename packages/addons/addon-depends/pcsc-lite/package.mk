1be506de9d0ded0deb130ce03c8ae1ce48f070e6
Rudi Heitbaum <rudi@heitbaum.com>
2024-05-09 04:51:03 +0000

    pcsc-lite: update to 2.2.2

--- packages/addons/addon-depends/pcsc-lite/package.mk
+++ packages/addons/addon-depends/pcsc-lite/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pcsc-lite"
-PKG_VERSION="2.1.0"
-PKG_SHA256="85cab61cc744c81e2bc432656863293b8428d0136f079e3b12a84b335b5b35aa"
+PKG_VERSION="2.2.2"
+PKG_SHA256="04edaed13565aab0fa7c711382be2b02e38faffbccd024ab68085ffb7091be22"
 PKG_LICENSE="GPL"
 PKG_SITE="https://pcsclite.apdu.fr"
-PKG_URL="https://pcsclite.apdu.fr/files/pcsc-lite-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://pcsclite.apdu.fr/files/pcsc-lite-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libusb polkit"
 PKG_LONGDESC="Middleware to access a smart card using SCard API (PC/SC)."
 
