d3273d22fda1c0c9ffaec3dd7e3a1bcc43ee62a9
Rudi Heitbaum <rudi@heitbaum.com>
2024-05-14 13:30:20 +0000

    stress-ng: update to 0.17.08

--- packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
+++ packages/addons/addon-depends/system-tools-depends/stress-ng/package.mk
@@ -2,11 +2,13 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="stress-ng"
-PKG_VERSION="0.17.07"
-PKG_SHA256="b0bc1495adce6c7a1f82d53f363682b243d6d7e93a06be7f94c9559c0a311a6f"
+PKG_VERSION="0.17.08"
+PKG_SHA256="e4982d2a1c57139dad1741d6248b00a30935f746c1f665ec5b9d53c010c8dc08"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/ColinIanKing/stress-ng"
 PKG_URL="https://github.com/ColinIanKing/stress-ng/archive/refs/tags/V${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain attr keyutils libaio libcap zlib"
 PKG_LONGDESC="stress-ng will stress test a computer system in various selectable ways"
 PKG_BUILD_FLAGS="-sysroot"
+
+TARGET_LDFLAGS+=" -lmpfr"
