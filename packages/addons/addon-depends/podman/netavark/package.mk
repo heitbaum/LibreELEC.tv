c5ca141abd0e3a891d7c6c6a6798291a3cfb677a
Rudi Heitbaum <rudi@heitbaum.com>
2024-05-31 22:22:14 +0000

    netavark: update to 1.11.0

--- packages/addons/addon-depends/podman/netavark/package.mk
+++ packages/addons/addon-depends/podman/netavark/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="netavark"
-PKG_VERSION="1.10.3"
-PKG_SHA256="fdc3010cb221f0fcef0302f57ef6f4d9168a61f9606238a3e1ed4d2e348257b7"
+PKG_VERSION="1.11.0"
+PKG_SHA256="5b96e5a00a41a550d716f1e5c180df6e0ee5b0ce20961827ef17aff3d6a92f9c"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/containers/netavark"
 PKG_URL="https://github.com/containers/netavark/archive/v${PKG_VERSION}.tar.gz"
