44ee40827fe913c0ebce1908c512242c16e6a900
Rudi Heitbaum <rudi@heitbaum.com>
2024-06-01 14:59:27 +0000

    mediainfo: update to 24.05

--- packages/addons/addon-depends/multimedia-tools-depends/mediainfo/package.mk
+++ packages/addons/addon-depends/multimedia-tools-depends/mediainfo/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mediainfo"
-PKG_VERSION="24.04"
-PKG_SHA256="ebeb1cb701a23059c736cb33b91a3172c4f98e7341e44a0b30a11919191a274d"
+PKG_VERSION="24.05"
+PKG_SHA256="b6451edc3d1bf54c17218e8e950818cf0e7b4913e6da28e9119400526a8e89f0"
 PKG_LICENSE="GPL"
 PKG_SITE="https://mediaarea.net/en/MediaInfo/Download/Source"
 PKG_URL="https://mediaarea.net/download/source/mediainfo/${PKG_VERSION}/mediainfo_${PKG_VERSION}.tar.xz"
