534d893085f253fc473496fdcb046c110e07814f
Rudi Heitbaum <rudi@heitbaum.com>
2024-06-01 01:58:54 +0000

    podman-patch-wip

--- packages/addons/addon-depends/podman/podman-bin/makepatch.sh
+++ packages/addons/addon-depends/podman/podman-bin/makepatch.sh
@@ -1,14 +1,31 @@
-tar xvf ../sources/podman-bin/podman-bin-5.0.0.tar.gz
-mv podman-5.0.0 a
-tar xvf ../sources/podman-bin/podman-bin-5.0.0.tar.gz
-mv podman-5.0.0 b
+#!/bin/bash
 
-cd b
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
+
+. ./config/options ""
+
+PKG_VERSION="$(get_pkg_version podman-bin)"
+
+unpack() {
+  mkdir -p ${PKG_BUILD}
+  tar --strip-components=1 -xf ${SOURCES}/llvm/llvm-${PKG_VERSION}.tar.xz -C ${PKG_BUILD}
+}
+
+TEMP_DIR=fg
+mkdir -p ${TEMP_DIR}/{a,b}
+
+tar --strip-components=1 -xf ${SOURCES}/podman-bin/podman-bin-${PKG_VERSION}.tar.gz -C ${TEMP_DIR}/a
+tar --strip-components=1 -xf ${SOURCES}/podman-bin/podman-bin-${PKG_VERSION}.tar.gz -C ${TEMP_DIR}/b
+
+cd ${TEMP_DIR}/b
 find . -name "*.go" -print | \
   xargs sed -i \
     -e '/^\W*\/\// ! s#/etc/containers#/storage/.kodi/addons/service.system.podman/etc/containers#g' \
+    -e '/^\W*\/\// ! s#/usr/local/etc/containers#/storage/.kodi/addons/service.system.podman/etc/containers#g' \
     -e '/^\W*\/\// ! s#/usr/share/containers#/storage/.kodi/userdata/addon_data/service.system.podman/podman/etc/containers#g' \
     -e '/^\W*\/\// ! s#/var/lib/containers#/storage/.kodi/userdata/addon_data/service.system.podman/podman#g'
 
-cd ..
-diff -Nur a b > podman-0002-path-changes.patch
+cd .. #${TEMP_DIR}
+pwd
+diff -Nur a b > podman-0002-path-changes.patch || true
