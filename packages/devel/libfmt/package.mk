9c4e480f0e9b340c876dafce284fff9b2d18109b
Rudi Heitbaum <rudi@heitbaum.com>
2024-07-04 07:42:34 +0000

    libfmt: update to 11.0.1

--- packages/devel/libfmt/package.mk
+++ packages/devel/libfmt/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libfmt"
-PKG_VERSION="10.2.1"
-PKG_SHA256="1250e4cc58bf06ee631567523f48848dc4596133e163f02615c97f78bab6c811"
+PKG_VERSION="11.0.1"
+PKG_SHA256="7d009f7f89ac84c0a83f79ed602463d092fbf66763766a907c97fd02b100f5e9"
 PKG_LICENSE="BSD"
 PKG_SITE="https://github.com/fmtlib/fmt"
 PKG_URL="https://github.com/fmtlib/fmt/archive/${PKG_VERSION}.tar.gz"
