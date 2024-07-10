4ea522a81985d35b354739c888418fb877bfc025
Rudi Heitbaum <rudi@heitbaum.com>
2024-07-10 13:06:12 +0000

    Revert "exiv2: meson WIP"
    This reverts commit 0f787685c6ec9e54b9742b82b1226cc1c51350b6.

--- packages/graphics/exiv2/package.mk
+++ packages/graphics/exiv2/package.mk
@@ -9,17 +9,19 @@ PKG_SITE="https://exiv2.org"
 PKG_URL="https://github.com/Exiv2/exiv2/archive/refs/tags/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Exiv2 is a Cross-platform C++ library to manage image metadata."
+PKG_TOOLCHAIN="cmake"
 
-PKG_MESON_OPTS_TARGET="-Ddefault_library=static \
-                       -Dwebready=false \
-                       -Dxmp=disabled \
-                       -Dcurl=disabled \
-                       -Dnls=disabled \
-                       -DEXIV2_BUILD_SAMPLES=disabled \
-                       -DunitTests=disabled \
-                       -Dvideo=false \
-                       -Dbmff=false \
-                       -Dbrotli=disabled \
-                       -Dinih=disabled \
-                       -Dfilesystem_access=OFF \
-                       -DEXIV2_BUILD_EXIV2_COMMAND=OFF"
+PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=OFF \
+                       -DEXIV2_ENABLE_WEBREADY=OFF \
+                       -DEXIV2_ENABLE_XMP=OFF \
+                       -DEXIV2_ENABLE_CURL=OFF \
+                       -DEXIV2_ENABLE_NLS=OFF \
+                       -DEXIV2_BUILD_SAMPLES=OFF \
+                       -DEXIV2_BUILD_UNIT_TESTS=OFF \
+                       -DEXIV2_ENABLE_VIDEO=OFF \
+                       -DEXIV2_ENABLE_BMFF=OFF \
+                       -DEXIV2_ENABLE_BROTLI=OFF \
+                       -DEXIV2_ENABLE_INIH=OFF \
+                       -DEXIV2_ENABLE_FILESYSTEM_ACCESS=OFF \
+                       -DEXIV2_BUILD_EXIV2_COMMAND=OFF \
+                       -DBUILD_WITH_CCACHE=ON"
