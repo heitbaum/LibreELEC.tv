f27de0a1933a78c75e514b05d39cdbfbbdf00dc6
Rudi Heitbaum <rudi@heitbaum.com>
2024-07-23 10:43:10 +0000

    cli: update to 27.1.1

--- packages/addons/addon-depends/docker/cli/package.mk
+++ packages/addons/addon-depends/docker/cli/package.mk
@@ -3,7 +3,7 @@
 
 PKG_NAME="cli"
 PKG_VERSION="$(get_pkg_version moby)"
-PKG_SHA256="f992e895c949852686abef9a6fa9efd622826c4f4d70b83876569a4641c4c8fc"
+PKG_SHA256="84db222b6d65695f3d8ae02acf8f21d90fb3f2169754bb94d314864c37bac7f3"
 PKG_LICENSE="ASL"
 PKG_SITE="https://github.com/docker/cli"
 PKG_URL="https://github.com/docker/cli/archive/v${PKG_VERSION}.tar.gz"
@@ -12,7 +12,7 @@ PKG_LONGDESC="The Docker CLI"
 PKG_TOOLCHAIN="manual"
 
 # Git commit of the matching tag https://github.com/docker/cli/tags
-export PKG_GIT_COMMIT="7d4bcd863a4c863e650eed02a550dfeb98560b83"
+export PKG_GIT_COMMIT="63125853e3a21c84f3f59eac6a0943e2a4008cf6"
 
 configure_target() {
   go_configure
