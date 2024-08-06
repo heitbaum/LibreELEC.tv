66051141afda06a26ea80896c5891caa20afbd90
Rudi Heitbaum <rudi@heitbaum.com>
2024-08-06 13:46:56 +0000

    bottom: update to 0.10.2

--- packages/addons/addon-depends/system-tools-depends/bottom/package.mk
+++ packages/addons/addon-depends/system-tools-depends/bottom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bottom"
-PKG_VERSION="0.10.1"
-PKG_SHA256="c0e507cc3a5246e65521e91391410efc605840abe3b40194c5769265051fa1cc"
+PKG_VERSION="0.10.2"
+PKG_SHA256="1db45fe9bc1fabb62d67bf8a1ea50c96e78ff4d2a5e25bf8ae8880e3ad5af80a"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/ClementTsang/bottom"
 PKG_URL="https://github.com/ClementTsang/bottom/archive/${PKG_VERSION}.tar.gz"
