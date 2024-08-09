b039da3b59502274c96df9b7532223e95414122b
Rudi Heitbaum <rudi@heitbaum.com>
2024-08-09 12:24:44 +0000

    PyYAML: update to 6.0.2
    log:
    - https://github.com/yaml/pyyaml/releases/tag/6.0.2

--- packages/python/devel/PyYAML/package.mk
+++ packages/python/devel/PyYAML/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="PyYAML"
-PKG_VERSION="6.0.1"
-PKG_SHA256="bfdf460b1736c775f2ba9f6a92bca30bc2095067b8a9d77876d1fad6cc3b4a43"
+PKG_VERSION="6.0.2"
+PKG_SHA256="d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
 PKG_LICENSE="MIT"
 PKG_SITE="https://pypi.org/project/PyYAML/"
-PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_URL="https://files.pythonhosted.org/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME,,}-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="Python3:host setuptools:host"
 PKG_LONGDESC="YAML parser and emitter for Python"
 PKG_TOOLCHAIN="manual"
