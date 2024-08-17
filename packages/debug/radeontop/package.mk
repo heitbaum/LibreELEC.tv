e4139c83451790fffc1d2272524808d31151ba39
Rudi Heitbaum <rudi@heitbaum.com>
2024-08-17 10:03:16 +0000

    radeontop: initial package

---
+++ packages/debug/radeontop/package.mk
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="radeontop"
+PKG_VERSION="ec97e6f11b106bcb1b3aadaa9a173afd61e3eb03"
+PKG_SHA256="5d9fb981615dbfadf7d3e21def2358a9db6f28c0dc87eca511a5777ef5328525"
+PKG_LICENSE="GPL-3.0-or-later"
+PKG_SITE="https://github.com/clbr/radeontop"
+PKG_URL="https://github.com/clbr/radeontop/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="View your GPU utilization, both for the total activity percent and individual blocks."
+
+export nls=0
+export xcb=0
