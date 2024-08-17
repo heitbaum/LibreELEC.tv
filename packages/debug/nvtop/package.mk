fac5374fd4c533adf71a8015f4eae4915fc26564
Rudi Heitbaum <rudi@heitbaum.com>
2024-08-17 10:12:08 +0000

    nvtop: update to 3.1.0

---
+++ packages/debug/nvtop/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="nvtop"
+PKG_VERSION="3.1.0"
+PKG_SHA256="9481c45c136163574f1f16d87789859430bc90a1dc62f181b269b5edd92f01f3"
+PKG_LICENSE="GPL-3.0-or-later"
+PKG_SITE="https://github.com/Syllo/nvtop"
+PKG_URL="https://github.com/Syllo/nvtop/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libdrm ncurses systemd"
+PKG_LONGDESC="GPU & Accelerator process monitoring for AMD, Apple, Huawei, Intel, NVIDIA and Qualcomm."
