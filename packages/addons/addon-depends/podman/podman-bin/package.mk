49114509354f2031e66a187ccb72972aff163900
Rudi Heitbaum <rudi@heitbaum.com>
2024-09-25 13:24:50 +0000

    podman-bin: update to 5.2.3

--- packages/addons/addon-depends/podman/podman-bin/package.mk
+++ packages/addons/addon-depends/podman/podman-bin/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="podman-bin"
-PKG_VERSION="5.2.2"
-PKG_SHA256="571658f175d61724269c1a20626c1e39424af59b7bcf7ff94135d03b790bbecb"
+PKG_VERSION="5.2.3"
+PKG_SHA256="48461fc0cd518119b6f45ed65bfd4da6dad353013d21ec3de225a0c7e4460bb7"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://podman.io/"
 PKG_URL="https://github.com/containers/podman/archive/v${PKG_VERSION}.tar.gz"
@@ -12,7 +12,7 @@ PKG_LONGDESC="Podman: A tool for managing OCI containers and pods."
 PKG_TOOLCHAIN="manual"
 
 # Git commit of the matching release https://github.com/containers/podman
-export PKG_GIT_COMMIT="fcee48106a12dd531702d729d17f40f6e152027f"
+export PKG_GIT_COMMIT="c5366a308e89edd9636b66faf79bd5cb18ed0905"
 
 PKG_PODMAN_BUILDTAGS="exclude_graphdriver_devicemapper \
                       exclude_graphdriver_btrfs \
