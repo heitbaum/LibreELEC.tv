e70aafa7a39476b084e41cf370dc41e277db8d67
Rudi Heitbaum <rudi@heitbaum.com>
2024-10-07 12:03:31 +0000

    talloc: initial package
    required by cifs-utils-7.1

---
+++ packages/devel/talloc/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="talloc"
+PKG_VERSION="2.4.2"
+PKG_SHA256="85ecf9e465e20f98f9950a52e9a411e14320bc555fa257d87697b7e7a9b1d8a6"
+PKG_LICENSE="LGPL-3.0-or-later"
+PKG_SITE="https://talloc.samba.org/"
+PKG_URL="https://www.samba.org/ftp/talloc/talloc-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="talloc is a hierarchical, reference counted memory pool system with destructors"
