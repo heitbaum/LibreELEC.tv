c913d67a5e3367ed062d922fd6d77534363bcbd4
Rudi Heitbaum <rudi@heitbaum.com>
2024-10-08 08:58:52 +0000

    Python3: update to 3.13.1

--- packages/lang/Python3/package.mk
+++ packages/lang/Python3/package.mk
@@ -3,8 +3,8 @@
 
 PKG_NAME="Python3"
 # When changing PKG_VERSION remember to sync PKG_PYTHON_VERSION!
-PKG_VERSION="3.12.8"
-PKG_SHA256="c909157bb25ec114e5869124cc2a9c4a4d4c1e957ca4ff553f1edc692101154e"
+PKG_VERSION="3.13.1"
+PKG_SHA256="9cf9427bee9e2242e3877dd0f6b641c1853ca461f39d6503ce260a59c80bf0d9"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.python.org/"
 PKG_URL="https://www.python.org/ftp/python/${PKG_VERSION}/${PKG_NAME::-1}-${PKG_VERSION}.tar.xz"
@@ -14,7 +14,7 @@ PKG_LONGDESC="Python3 is an interpreted object-oriented programming language."
 PKG_BUILD_FLAGS="-cfg-libs -cfg-libs:host"
 PKG_TOOLCHAIN="autotools"
 
-PKG_PYTHON_VERSION="python3.12"
+PKG_PYTHON_VERSION="python3.13"
 
 PKG_CONFIGURE_OPTS_HOST="ac_cv_prog_HAS_HG=/bin/false
                          ac_cv_prog_SVNVERSION=/bin/false
@@ -39,7 +39,6 @@ PKG_CONFIGURE_OPTS_HOST="ac_cv_prog_HAS_HG=/bin/false
                          --disable-curses
                          --disable-pydoc
                          --disable-test-modules
-                         --disable-lib2to3
                          --disable-idle3
                          --with-expat=builtin
                          --with-doc-strings
@@ -78,7 +77,6 @@ PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_HAS_HG=/bin/false
                            --enable-curses
                            --disable-pydoc
                            --disable-test-modules
-                           --disable-lib2to3
                            --disable-idle3
                            --with-expat=system
                            --with-doc-strings
@@ -121,7 +119,7 @@ post_makeinstall_target() {
 
   PKG_INSTALL_PATH_LIB=${INSTALL}/usr/lib/${PKG_PYTHON_VERSION}
 
-  for dir in config compiler sysconfigdata lib-dynload/sysconfigdata lib2to3/tests test; do
+  for dir in config compiler sysconfigdata lib-dynload/sysconfigdata test; do
     rm -rf ${PKG_INSTALL_PATH_LIB}/${dir}
   done
 
