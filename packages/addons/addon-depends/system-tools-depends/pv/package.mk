887f57b6ac3da2b309cb47c749696015c4575297
Rudi Heitbaum <rudi@heitbaum.com>
2024-10-16 08:18:13 +0000

    pv: update to 1.9.0

--- packages/addons/addon-depends/system-tools-depends/pv/package.mk
+++ packages/addons/addon-depends/system-tools-depends/pv/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pv"
-PKG_VERSION="1.8.14"
-PKG_SHA256="0cc18811a4809a587d4b11d47691bbc0ad83a5d95d2c2606af74ea7b4a674756"
+PKG_VERSION="1.9.0"
+PKG_SHA256="b38d69d7fc0785eb5eb3c57e8b12a7334f862047bf84b18f414365335399469a"
 PKG_LICENSE="GNU"
 PKG_SITE="http://www.ivarch.com/programs/pv.shtml"
 PKG_URL="http://www.ivarch.com/programs/sources/pv-${PKG_VERSION}.tar.gz"
