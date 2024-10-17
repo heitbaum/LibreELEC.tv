797d72a00eb534428f7b2b50429128737059c6fb
Rudi Heitbaum <rudi@heitbaum.com>
2024-10-17 03:26:01 +0000

    syncthing: update to 1.28.0 and addon (2)

--- packages/addons/service/syncthing/package.mk
+++ packages/addons/service/syncthing/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="syncthing"
-PKG_VERSION="1.27.12"
-PKG_SHA256="5531f0e1bb81b824a2ab62f070e745c142e1328a15229de47b0cb596b5bae417"
-PKG_REV="1"
+PKG_VERSION="1.28.0"
+PKG_SHA256="73b4030f9fca381f58f4966db48cc135cd8232fe9e8853b5651de0f0bf4cfbf7"
+PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="MPLv2"
 PKG_SITE="https://syncthing.net/"
