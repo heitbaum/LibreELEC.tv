6f1b87015b835ac4e1d52e4367986ac022a39283
Rudi Heitbaum <rudi@heitbaum.com>
2024-10-18 06:36:46 +0000

    libimobiledevice-glue: update to 1.3.1

--- packages/addons/addon-depends/libimobiledevice-glue/package.mk
+++ packages/addons/addon-depends/libimobiledevice-glue/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libimobiledevice-glue"
-PKG_VERSION="1.3.0"
-PKG_SHA256="96ec4eb2b1e217392149eafb2b5c3cd3e7110200f0e2bb5003c37d3ead7244ef"
+PKG_VERSION="1.3.1"
+PKG_SHA256="6e2849f221e6ab970566a115d42f3c20f8848e4d40c2ed61ac20dc85f40fa54f"
 PKG_LICENSE="LGPL-2.1"
 PKG_SITE="http://www.libimobiledevice.org"
 PKG_URL="https://github.com/libimobiledevice/libimobiledevice-glue/releases/download/${PKG_VERSION}/libimobiledevice-glue-${PKG_VERSION}.tar.bz2"
