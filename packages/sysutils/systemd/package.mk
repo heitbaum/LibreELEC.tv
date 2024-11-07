cabe534b62b4c92f5c8ca5c20ed098b35550134c
Rudi Heitbaum <rudi@heitbaum.com>
2024-11-07 07:48:03 +0000

    systemd: update to 257-rc2
    Release notes:
    - https://github.com/systemd/systemd/releases
    - https://github.com/systemd/systemd/releases/tag/v257-rc1
    - https://github.com/systemd/systemd/releases/tag/v257-rc2

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="systemd"
-PKG_VERSION="256.8"
-PKG_SHA256="b3d003b4f6d1ab0bfae0cf7a37c4aa559923c49bc8e9d1331b7459e12ebc357a"
+PKG_VERSION="257-rc2"
+PKG_SHA256="6ce893e3fb469bfad0a3b5e3033e0c52bfac8d26875358efc4d8b4fb19275ab9"
 PKG_LICENSE="LGPL2.1+"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/systemd"
 PKG_URL="https://github.com/systemd/systemd/archive/v${PKG_VERSION}.tar.gz"
