51402587ccb5faf2fdb3f30f0c98c73897cfdab4
Rudi Heitbaum <rudi@heitbaum.com>
2024-11-09 20:36:18 +0000

    curl: update to 8.11.1
    Release notes:
    - https://curl.se/changes.html

--- packages/web/curl/package.mk
+++ packages/web/curl/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="curl"
-PKG_VERSION="8.10.1"
-PKG_SHA256="73a4b0e99596a09fa5924a4fb7e4b995a85fda0d18a2c02ab9cf134bebce04ee"
+PKG_VERSION="8.11.1"
+PKG_SHA256="c7ca7db48b0909743eaef34250da02c19bc61d4f1dcedd6603f109409536ab56"
 PKG_LICENSE="MIT"
 PKG_SITE="https://curl.haxx.se"
 PKG_URL="https://curl.haxx.se/download/${PKG_NAME}-${PKG_VERSION}.tar.xz"
