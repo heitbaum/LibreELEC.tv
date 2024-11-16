1010f7ce88d80190debce37db769b177daf05cc7
Rudi Heitbaum <rudi@heitbaum.com>
2024-11-16 02:15:54 +0000

    libnvme: update to 1.11.1

--- packages/tools/libnvme/package.mk
+++ packages/tools/libnvme/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libnvme"
-PKG_VERSION="1.11"
-PKG_SHA256="f6da60036b14e0427246718a32abff442228331f9ae3853f89160cf366d83dfe"
+PKG_VERSION="1.11.1"
+PKG_SHA256="6d5d8ba2cc4c94a61a994c9f7f25b3b26ef973fb5c0daa37729890903f37a1f1"
 PKG_LICENSE="GPL"
 PKG_SITE="https://nvmexpress.org"
 PKG_URL="https://github.com/linux-nvme/libnvme/archive/refs/tags/v${PKG_VERSION}.tar.gz"
