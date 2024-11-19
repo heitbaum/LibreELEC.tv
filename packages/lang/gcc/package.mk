e092f9984d9b1732316eb08eebc48856e8508fb9
Rudi Heitbaum <rudi@heitbaum.com>
2024-11-19 12:13:52 +0000

    gcc: update to 15-20250126

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -3,11 +3,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="gcc"
-PKG_VERSION="14.2.0"
-PKG_SHA256="a7b39bc69cbf9e25826c5a60ab26477001f7c08d85cec04bc0e29cabed6f3cc9"
+PKG_VERSION="15-20250126"
+PKG_SHA256="8d57151f77db361e3a5fba8ee83c60515cb273496824f8f4280e042c764043b5"
 PKG_LICENSE="GPL-2.0-or-later"
 PKG_SITE="https://gcc.gnu.org/"
 PKG_URL="https://ftpmirror.gnu.org/gcc/${PKG_NAME}-${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_URL="https://gcc.gnu.org/pub/gcc/snapshots/${PKG_VERSION}/gcc-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_BOOTSTRAP="ccache:host autoconf:host binutils:host gmp:host mpfr:host mpc:host zstd:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_DEPENDS_HOST="ccache:host autoconf:host binutils:host gmp:host mpfr:host mpc:host zstd:host glibc libxcrypt"
