8c11b5f3fef4586430be778dad69ea8186c18a59
Rudi Heitbaum <rudi@heitbaum.com>
2024-11-23 21:45:16 +0000

    nvtop: update to githash 6e91c74

--- packages/debug/nvtop/package.mk
+++ packages/debug/nvtop/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nvtop"
-PKG_VERSION="3.1.0"
-PKG_SHA256="9481c45c136163574f1f16d87789859430bc90a1dc62f181b269b5edd92f01f3"
+PKG_VERSION="6e91c745cd051d902fc57e9195068df03eb192bb"
+PKG_SHA256="dd162f984893c63b67c07113dec5fb327b6f643889dadd0ced69db4184523ffa"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/Syllo/nvtop"
 PKG_URL="https://github.com/Syllo/nvtop/archive/${PKG_VERSION}.tar.gz"
