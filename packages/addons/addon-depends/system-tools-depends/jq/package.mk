3becf3c161fac71ef50080ba7eb87ebe46567fdf
Rudi Heitbaum <rudi@heitbaum.com>
2024-12-09 12:56:42 +0000

    jq: temp hack update to githash 8bcdc93 for gcc-15

--- packages/addons/addon-depends/system-tools-depends/jq/package.mk
+++ packages/addons/addon-depends/system-tools-depends/jq/package.mk
@@ -2,14 +2,16 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="jq"
-PKG_VERSION="1.7.1"
-PKG_SHA256="478c9ca129fd2e3443fe27314b455e211e0d8c60bc8ff7df703873deeee580c2"
+PKG_VERSION="8bcdc9304ace5f2cc9bf662ab8998d75537e05f0"
+PKG_SHA256="f5bca864d0d9c28c330a93a790e19d0cd2a4fc58cfa94179d38e2492a3f03fd0"
 PKG_LICENSE="MIT"
 PKG_SITE="https://jqlang.github.io/jq/"
 PKG_URL="https://github.com/jqlang/jq/releases/download/${PKG_NAME}-${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/jqlang/jq/archive/refs/heads/master.tar.gz"
 PKG_DEPENDS_TARGET="toolchain oniguruma"
 PKG_LONGDESC="Command-line JSON processor"
 PKG_BUILD_FLAGS="-sysroot"
+PKG_TOOLCHAIN="autotools"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
                            --enable-static \
