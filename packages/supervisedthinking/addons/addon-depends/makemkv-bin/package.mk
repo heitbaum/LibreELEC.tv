a4870473e190ef088f7afabb78bbeafbf60ff441
Rudi Heitbaum <rudi@heitbaum.com>
2024-12-16 01:08:13 +0000

    makemkv-bin: update to 1.17.8

--- packages/supervisedthinking/addons/addon-depends/makemkv-bin/package.mk
+++ packages/supervisedthinking/addons/addon-depends/makemkv-bin/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="makemkv-bin"
-PKG_VERSION="1.17.5"
-PKG_SHA256="cb009c31f69601e2f66e315925a09ad175bad041f27c5096d7b06dd5004df04f"
+PKG_VERSION="1.17.8"
+PKG_SHA256="8e0f547430d9afeed976c789b5becdfb2ecfae1caade12e8e3e119a734717041"
 PKG_ARCH="aarch64 arm x86_64"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.makemkv.com/"
