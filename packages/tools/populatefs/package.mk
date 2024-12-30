b520c99600ebd8833eb33dad376bbb7502afbc40
Rudi Heitbaum <rudi@heitbaum.com>
2024-12-30 10:27:03 +0000

    populatefs: update to githash 8d7fd68
    update to "current" fork which includes the previously local patch

--- packages/tools/populatefs/package.mk
+++ packages/tools/populatefs/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="populatefs"
-PKG_VERSION="1.0"
-PKG_SHA256="e5845404188b5da3afb11229ecb38646cc1562b61400035774dbc237c3b706d2"
+PKG_VERSION="8d7fd68786d20eb40337da5dbae3992fbda78317"
+PKG_SHA256="c5e46e174a25ebf64b158a80b2c1579e7fbe5c7173a0a4cd767a2a6ff4356736"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/lipnitsk/populatefs"
-PKG_URL="https://github.com/lipnitsk/${PKG_NAME}/archive/v${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/kfix/populatefs"
+PKG_URL="https://github.com/kfix/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="e2fsprogs:host"
 PKG_LONGDESC="populatefs: Tool for replacing genext2fs when creating ext4 images"
 PKG_BUILD_FLAGS="+pic:host"
