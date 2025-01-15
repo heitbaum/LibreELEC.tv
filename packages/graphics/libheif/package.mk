0c7fe125741781807ee68538d8b0a86a0a3a875e
Rudi Heitbaum <rudi@heitbaum.com>
2025-01-15 02:39:12 +0000

    libheif: update to githash 3cdb3cc
    Release notes:
    - https://github.com/strukturag/libheif/releases/tag/vgithash 3cdb3cc

--- packages/graphics/libheif/package.mk
+++ packages/graphics/libheif/package.mk
@@ -2,11 +2,12 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libheif"
-PKG_VERSION="1.19.5"
-PKG_SHA256="d3cf0a76076115a070f9bc87cf5259b333a1f05806500045338798486d0afbaf"
+PKG_VERSION="3cdb3cc40d195f852422987a035c26e89950d74c"
+PKG_SHA256="9c7bba01eff045237b14be42e6a2c386e54ec2f117475da19bb39a127c36a863"
 PKG_LICENSE="LGPLv3"
 PKG_SITE="https://www.libde265.org"
 PKG_URL="https://github.com/strukturag/libheif/releases/download/v${PKG_VERSION}/libheif-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/strukturag/libheif/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libde265 libjpeg-turbo libpng"
 PKG_LONGDESC="A HEIF file format decoder and encoder."
 PKG_BUILD_FLAGS="+pic"
