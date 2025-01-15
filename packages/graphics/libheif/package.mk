28c2b810fa7aa2e730d179f721bc7b6ee474afe0
Rudi Heitbaum <rudi@heitbaum.com>
2025-01-15 02:39:12 +0000

    libheif: update to githash e52035e
    Release notes:
    - https://github.com/strukturag/libheif/releases/tag/vgithash e52035e

--- packages/graphics/libheif/package.mk
+++ packages/graphics/libheif/package.mk
@@ -2,11 +2,12 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libheif"
-PKG_VERSION="1.19.5"
-PKG_SHA256="d3cf0a76076115a070f9bc87cf5259b333a1f05806500045338798486d0afbaf"
+PKG_VERSION="e52035eaeb0697c09f4d70dd52c02281b51f2a0a"
+PKG_SHA256="46efbade8a54e97523ed43210cafb344998460910a9118c44a6aafbd8c9737d3"
 PKG_LICENSE="LGPLv3"
 PKG_SITE="https://www.libde265.org"
 PKG_URL="https://github.com/strukturag/libheif/releases/download/v${PKG_VERSION}/libheif-${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/strukturag/libheif/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain libde265 libjpeg-turbo libpng"
 PKG_LONGDESC="A HEIF file format decoder and encoder."
 PKG_BUILD_FLAGS="+pic"
