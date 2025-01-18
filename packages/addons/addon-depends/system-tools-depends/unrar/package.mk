fc7b1ff7cb29afc5789c72275e9fc288b5cc2116
Rudi Heitbaum <rudi@heitbaum.com>
2025-01-18 05:26:17 +0000

    unrar: update to 7.1.3

--- packages/addons/addon-depends/system-tools-depends/unrar/package.mk
+++ packages/addons/addon-depends/system-tools-depends/unrar/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="unrar"
-PKG_VERSION="7.1.2"
-PKG_SHA256="143dcacb3a9d9ca0af695adecabe1350b0a3e93d927a065ef8f7cc605ab2d322"
+PKG_VERSION="7.1.3"
+PKG_SHA256="f7edb6f55fb53611206781d9e56f2625ef4411a6b129768800196617d9df920a"
 PKG_LICENSE="free"
 PKG_SITE="https://www.rarlab.com/rar_add.htm"
 PKG_URL="https://www.rarlab.com/rar/unrarsrc-${PKG_VERSION}.tar.gz"
