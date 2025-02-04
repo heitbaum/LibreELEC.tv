f6fbc86966a54cf7d2147a87c56961242eabb476
Rudi Heitbaum <rudi@heitbaum.com>
2025-02-04 09:27:08 +0000

    vdr-plugin-live: update to 3.4.1

--- packages/addons/addon-depends/vdr-plugins/vdr-plugin-live/package.mk
+++ packages/addons/addon-depends/vdr-plugins/vdr-plugin-live/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vdr-plugin-live"
-PKG_VERSION="3.4.0"
-PKG_SHA256="ab3e40103cc102c0c6c18ab86c89ef0bcfadbf04f5972f76f32caef5224bb8ec"
+PKG_VERSION="3.4.1"
+PKG_SHA256="8b9f88f850aadddc0ccf08d9e27a8fa1048a627ded4dda0154ac185d3eb56968"
 PKG_LICENSE="GPL"
 PKG_SITE="http://live.vdr-developer.org/en/index.php"
 PKG_URL="https://github.com/MarkusEh/vdr-plugin-live/archive/v${PKG_VERSION}.tar.gz"
