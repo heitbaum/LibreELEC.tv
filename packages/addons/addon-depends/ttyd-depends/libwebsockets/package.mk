f71d1a5d441faae80cd55a05f1e9461929c37257
Rudi Heitbaum <rudi@heitbaum.com>
2025-02-26 20:37:11 +0000

    libwebsockets: update to 4.3.4

--- packages/addons/addon-depends/ttyd-depends/libwebsockets/package.mk
+++ packages/addons/addon-depends/ttyd-depends/libwebsockets/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libwebsockets"
-PKG_VERSION="4.3.3"
-PKG_SHA256="6fd33527b410a37ebc91bb64ca51bdabab12b076bc99d153d7c5dd405e4bdf90"
+PKG_VERSION="4.3.4"
+PKG_SHA256="896b36aa063b4d05865f9ffee4404b26d4c2d3e2ba17b0b69f021b615377845e"
 PKG_LICENSE="MIT"
 PKG_SITE="https://libwebsockets.org"
 PKG_URL="https://github.com/warmcat/libwebsockets/archive/v${PKG_VERSION}.tar.gz"
