7a0a1aaffe20f7aac0d3f34ec02b7e66b044d383
Rudi Heitbaum <rudi@heitbaum.com>
2025-04-09 10:19:53 +0000

    Python3: update to 3.13.3

--- packages/lang/Python3/package.mk
+++ packages/lang/Python3/package.mk
@@ -3,8 +3,8 @@
 
 PKG_NAME="Python3"
 # When changing PKG_VERSION remember to sync PKG_PYTHON_VERSION!
-PKG_VERSION="3.13.2"
-PKG_SHA256="d984bcc57cd67caab26f7def42e523b1c015bbc5dc07836cf4f0b63fa159eb56"
+PKG_VERSION="3.13.3"
+PKG_SHA256="40f868bcbdeb8149a3149580bb9bfd407b3321cd48f0be631af955ac92c0e041"
 PKG_LICENSE="OSS"
 PKG_SITE="https://www.python.org/"
 PKG_URL="https://www.python.org/ftp/python/${PKG_VERSION}/${PKG_NAME::-1}-${PKG_VERSION}.tar.xz"
