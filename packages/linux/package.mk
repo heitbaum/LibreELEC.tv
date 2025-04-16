bd6368a55daeeaec56378144fa53f821c78a5ea8
Rudi Heitbaum <rudi@heitbaum.com>
2025-04-16 13:36:49 +0000

    linux: update to 6.14.2

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -30,8 +30,8 @@ case "${LINUX}" in
     PKG_PATCH_DIRS="raspberrypi rtlwifi/6.13 rtlwifi/6.14 rtlwifi/after-6.14"
     ;;
   *)
-    PKG_VERSION="6.14.1"
-    PKG_SHA256="5bf122d1879fd64fadf0cecfcd477957ebce1bc5931c14835ee0eda88463e407"
+    PKG_VERSION="6.14.2"
+    PKG_SHA256="c5c682a354ea3190139357a57d34a79e5c37221ace823a938e10116b577a2e1b"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default rtlwifi/after-6.14"
     ;;
