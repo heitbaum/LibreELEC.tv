6970a98266d36851bd14b3afb39145fa7616b543
Rudi Heitbaum <rudi@heitbaum.com>
2025-04-19 04:10:48 +0000

    Revert "grub: gcc15 workaround"
    This reverts commit 75eec1ad9981028e3307aa1286cf22ddd5356918.

--- packages/tools/grub/package.mk
+++ packages/tools/grub/package.mk
@@ -60,7 +60,7 @@ make_target() {
   make CC=${CC} \
        AR=${AR} \
        RANLIB=${RANLIB} \
-       CFLAGS="-I${SYSROOT_PREFIX}/usr/include -fomit-frame-pointer -D_FILE_OFFSET_BITS=64 -Wno-error=unterminated-string-initialization" \
+       CFLAGS="-I${SYSROOT_PREFIX}/usr/include -fomit-frame-pointer -D_FILE_OFFSET_BITS=64" \
        LDFLAGS="-L${SYSROOT_PREFIX}/usr/lib"
 }
 
