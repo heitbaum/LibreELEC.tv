12e292a4b3625d80bab3db1fc60252172c39cd53
Rudi Heitbaum <rudi@heitbaum.com>
2025-05-19 03:08:41 +0000

    libretro-scummvm: update to githash 9d31b31

--- packages/emulation/libretro-scummvm/package.mk
+++ packages/emulation/libretro-scummvm/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-scummvm"
-PKG_VERSION="7310d4e9f5d11553c6c5499911bd2f9b8ff3db3b"
-PKG_SHA256="c764691df32d3670db2d5e90bb520940a0a4c4ceeed7e73b2faed50c8ab9aa13"
+PKG_VERSION="9d31b31c179fd4a43f7cfc383a3435a9070c6aa8"
+PKG_SHA256="a0450024ce6e6156c27c69018d86dfa409639e260c8564ee0da6920d4011f141"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/scummvm"
 PKG_URL="https://github.com/libretro/scummvm/archive/${PKG_VERSION}.tar.gz"
