2a6b529da3a19030e30b2a232c8de74e53f8fcdb
Rudi Heitbaum <rudi@heitbaum.com>
2025-06-22 02:32:30 +0000

    libgudev: update to 238

---
+++ packages/sysutils/libgudev/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libgudev"
+PKG_VERSION="238"
+PKG_SHA256="61266ab1afc9d73dbc60a8b2af73e99d2fdff47d99544d085760e4fa667b5dd1"
+PKG_LICENSE="LGPLv2.1"
+PKG_SITE="https://gitlab.gnome.org/GNOME/libgudev/"
+PKG_URL="https://download.gnome.org/sources/libgudev/${PKG_VERSION}/libgudev-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain glib systemd"
+PKG_LONGDESC="A library providing GObject bindings for libudev"
