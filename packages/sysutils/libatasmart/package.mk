64380d17fb0ea46b3d139e0891ad8c32bed1940f
Rudi Heitbaum <rudi@heitbaum.com>
2025-06-22 04:01:28 +0000

    libatasmart: update to 0.19

---
+++ packages/sysutils/libatasmart/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libatasmart"
+PKG_VERSION="0.19"
+PKG_SHA256="61f0ea345f63d28ab2ff0dc352c22271661b66bf09642db3a4049ac9dbdb0f8d"
+PKG_LICENSE="LGPLv2.1"
+PKG_SITE="http://0pointer.de/blog/projects/being-smart.html"
+PKG_URL="http://0pointer.de/public/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain systemd"
+PKG_LONGDESC="ATA S.M.A.R.T. Reading and Parsing Library"
