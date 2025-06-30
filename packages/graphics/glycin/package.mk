1e25578ffde6203e5afa47995a5377e4b95812f9
Rudi Heitbaum <rudi@heitbaum.com>
2025-06-30 14:09:19 +0000

    glycin: initial package
    https://doc.rust-lang.org/cargo/reference/environment-variables.html
    
    required by: gdk-pixbuf: update to 2.43.3

---
+++ packages/graphics/glycin/package.mk
@@ -0,0 +1,50 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="glycin"
+PKG_VERSION="2.0.4"
+PKG_SHA256="1ad3f6c9e6a1819d172f18de6e23d9ab7480baf8b93c5ade32190e882176fda3"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.gtk.org/"
+PKG_URL="https://gitlab.gnome.org/GNOME/glycin/-/archive/${PKG_VERSION}/glycin-${PKG_VERSION}.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain cargo:host fontconfig libseccomp lcms2 glib"
+PKG_LONGDESC="Sandboxed and extendable image loading"
+PKG_BUILD_FLAGS="-sysroot"
+
+PKG_MESON_PROPERTIES_TARGET="skip_sanity_check = true"
+
+PKG_MESON_OPTS_TARGET="--wrap-mode=nodownload \
+                       -Dglycin-loaders=false \
+                       -Dlibglycin-gtk4=false \
+                       -Dintrospection=false \
+                       -Dtests=false \
+                       -Dglycin-thumbnailer=false \
+                       -Dvapi=false"
+
+if [ "${DISPLAYSERVER}" = "x11" ]; then
+  PKG_DEPENDS_TARGET+=" libX11"
+fi
+
+pre_configure_target() {
+  export PKG_CONFIG_PATH="$(get_install_dir libseccomp)/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
+  #export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=${TARGET_CC}
+  #export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=${TARGET_CC}
+
+  case "${ARCH}" in
+    "aarch64")
+      RUST_TARGET="aarch64-libreelec-linux-gnu.json"
+      ;;
+    "arm")
+      RUST_TARGET="armv7ve-libreelec-linux-gnueabihf.json"
+      ;;
+    "x86_64")
+      RUST_TARGET="x86_64-libreelec-linux-gnu.json"
+      ;;
+  esac
+
+  mkdir cargo-home
+  cat > cargo-home/config.toml <<EOF
+[build]
+target="${TOOLCHAIN}/lib/rustlib/${RUST_TARGET}"
+EOF
+}
