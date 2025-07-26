fe49fd15b86e53ec5ec5e586099371ae7943adc8
Rudi Heitbaum <rudi@heitbaum.com>
2025-07-26 00:20:16 +0000

    tpm2-tools: update to 5.7

--- packages/security/tpm2-tools/package.mk
+++ packages/security/tpm2-tools/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tpm2-tools"
-PKG_VERSION="5.5"
-PKG_SHA256="1fdb49c730537bfdaed088884881a61e3bfd121e957ec0bdceeec0261236c123"
+PKG_VERSION="5.7"
+PKG_SHA256="3810d36b5079256f4f2f7ce552e22213d43b1031c131538df8a2dbc3c570983a"
 PKG_LICENSE="BSD-3-Clause"
 PKG_SITE="https://tpm2-software.github.io"
 PKG_URL="https://github.com/tpm2-software/tpm2-tools/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
