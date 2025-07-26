5b267fdd8bf0df86a068345aa093afded54f82cd
Rudi Heitbaum <rudi@heitbaum.com>
2025-07-26 00:20:32 +0000

    tpm2-tss: update to 4.1.3

--- packages/security/tpm2-tss/package.mk
+++ packages/security/tpm2-tss/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="tpm2-tss"
-PKG_VERSION="4.0.1"
-PKG_SHA256="532a70133910b6bd842289915b3f9423c0205c0ea009d65294ca18a74087c950"
+PKG_VERSION="4.1.3"
+PKG_SHA256="37f1580200ab78305d1fc872d89241aaee0c93cbe85bc559bf332737a60d3be8"
 PKG_LICENSE="BSD-3-Clause"
 PKG_SITE="https://tpm2-software.github.io"
 PKG_URL="https://github.com/tpm2-software/tpm2-tss/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
