d7ccece181c45777d9b61b0318f77c4ed6969dd8
Rudi Heitbaum <rudi@heitbaum.com>
2025-11-08 10:54:29 +0000

    zlib: build with -fPIC
    Required by fwupd via libarchive

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -12,6 +12,7 @@ PKG_DEPENDS_HOST="ccache:host cmake:host"
 PKG_DEPENDS_TARGET="cmake:host gcc:host"
 PKG_LONGDESC="zlib data compression library for the next generation systems"
 PKG_TOOLCHAIN="cmake-make"
+PKG_BUILD_FLAGS="+pic"
 
 PKG_CMAKE_OPTS_HOST="-DZLIB_COMPAT=ON"
 
