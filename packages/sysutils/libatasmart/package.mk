e70df28a253576e464efa7b26293d4b7096426e1
Rudi Heitbaum <rudi@heitbaum.com>
2025-11-08 11:24:09 +0000

    libatasmart: update to githash 9bf6927 and PKG_URL

--- packages/sysutils/libatasmart/package.mk
+++ packages/sysutils/libatasmart/package.mk
@@ -2,15 +2,21 @@
 # Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libatasmart"
-PKG_VERSION="0.19"
-PKG_SHA256="61f0ea345f63d28ab2ff0dc352c22271661b66bf09642db3a4049ac9dbdb0f8d"
+PKG_VERSION="9bf6927c02978bb2f08c02c4cf45f3f3b154e41f"
+PKG_SHA256="4799ce3a82e131d671da67c5611e4e3d8c9b575d181869441f5b25767c38df64"
 PKG_LICENSE="LGPLv2.1"
 PKG_SITE="http://0pointer.de/blog/projects/being-smart.html"
-PKG_URL="http://0pointer.de/public/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain systemd"
+PKG_URL="https://github.com/libatasmart/libatasmart/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST="toolchain:host"
+PKG_DEPENDS_TARGET="toolchain systemd libatasmart:host"
 PKG_LONGDESC="ATA S.M.A.R.T. Reading and Parsing Library"
 
-pre_make_target() {
-  # building host tools seems to be broken in 0.19, prebuild these
-  make CFLAGS="${HOST_CFLAGS}" LDFLAGS="${HOST_LDFLAGS}" CC="${HOST_CC}" -C strpool
+PKG_MESON_OPTS_HOST="-Dstrpool_build=true \
+                     -Dsrc_build=false"
+
+PKG_MESON_OPTS_TARGET="-Dstrpool_build=false \
+                       -Dsrc_build=true"
+
+pre_configure_target() {
+  PKG_MESON_OPTS_TARGET+=" -Dstrpool_path=${PKG_BUILD}/.${HOST_NAME}/tools/strpool/strpool"
 }
