b51b0f7dca762dfb5689c8c51415699fef5e8c9c
Rudi Heitbaum <rudi@heitbaum.com>
2025-11-29 22:48:55 +0000

    cryptsetup: update to 2.8.1 and meson

--- packages/sysutils/cryptsetup/package.mk
+++ packages/sysutils/cryptsetup/package.mk
@@ -2,22 +2,20 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="cryptsetup"
-PKG_VERSION="2.6.1"
-PKG_SHA256="410ded65a1072ab9c8e41added37b9729c087fef4d2db02bb4ef529ad6da4693"
+PKG_VERSION="2.8.1"
+PKG_SHA256="2c3379eb76597dcab50911449b013e2697c4bffcc716dbbf0d9b0e8fbbb46fb4"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://gitlab.com/cryptsetup/cryptsetup"
-PKG_URL="https://www.kernel.org/pub/linux/utils/cryptsetup/v2.6/cryptsetup-$PKG_VERSION.tar.xz"
+PKG_URL="https://www.kernel.org/pub/linux/utils/cryptsetup/v2.8/cryptsetup-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libgcrypt popt json-c util-linux openssl lvm2-lib"
 PKG_SECTION="sysutils"
 PKG_SHORTDESC="Userspace setup tool for transparent encryption of block devices using dm-crypt"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-fips \
-                           --disable-veritysetup \
-                           --disable-cryptsetup-reencrypt \
-                           --disable-integritysetup \
-                           --disable-asciidoc \
-                           --disable-ssh-token \
-                           --enable-gcrypt-pbkdf2 \
-                           --with-sysroot=$SYSROOT_PREFIX \
-                           --with-libgcrypt-prefix=$SYSROOT_PREFIX/usr"
+PKG_MESON_OPTS_TARGET="-Dfips=true \
+                       -Dveritysetup=false \
+                       -Dluks2-reencryption=false \
+                       -Dintegritysetup=false \
+                       -Dasciidoc=disabled \
+                       -Dssh-token=false \
+                       -Dgcrypt-pbkdf2=enabled"
