7ae66359bd4e92536b521ca96c3d10849cef9efb
Rudi Heitbaum <rudi@heitbaum.com>
2025-11-29 22:50:29 +0000

    systemd: add version 260 dependency of cryptsetup
    MVP to support 260 from https://forum.libreelec.tv/thread/27952-le-12-added-lvm2-luks-dm-crypt-veracrypt-bitlocker-mdraid-ext4-encryption/

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="3f8d3d3969af7214bda600930e14c6a24135eb3dce1ba7f1b980b74e6dc15b72"
 PKG_LICENSE="LGPL-2.1-or-later"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/systemd"
 PKG_URL="https://github.com/systemd/systemd/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="meson:host ninja:host gcc:host libcap kmod util-linux entropy libidn2 wait-time-sync Jinja2:host"
+PKG_DEPENDS_TARGET="meson:host ninja:host gcc:host cryptsetup libcap kmod util-linux entropy libidn2 wait-time-sync Jinja2:host"
 PKG_LONGDESC="A system and session manager for Linux, compatible with SysV and LSB init scripts."
 PKG_BUILD_FLAGS="+lto"
 
@@ -28,7 +28,7 @@ PKG_MESON_OPTS_TARGET="--libdir=/usr/lib \
                        -Dpam=disabled \
                        -Dpwquality=disabled \
                        -Dmicrohttpd=disabled \
-                       -Dlibcryptsetup=disabled \
+                       -Dlibcryptsetup=enabled \
                        -Dlibcurl=disabled \
                        -Dlibidn2=enabled \
                        -Dqrencode=disabled \
