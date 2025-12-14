db98d4a3d54009cbdcd4a8a1746aba9951ea677c
Rudi Heitbaum <rudi@heitbaum.com>
2025-12-14 06:04:39 +0000

    Revert "systemd: add version 260 dependency of cryptsetup"
    This reverts commit ec685a445c190fa375644769fffb19ecc369cc28.

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="6aa77506c0644aa67f940a48e3d3a7368601f787e4f249139516d353f107bcab"
 PKG_LICENSE="LGPL-2.1-or-later"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/systemd"
 PKG_URL="https://github.com/systemd/systemd/archive/v${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="meson:host ninja:host gcc:host cryptsetup libcap kmod util-linux entropy libidn2 wait-time-sync Jinja2:host"
+PKG_DEPENDS_TARGET="meson:host ninja:host gcc:host libcap kmod util-linux entropy libidn2 wait-time-sync Jinja2:host"
 PKG_LONGDESC="A system and session manager for Linux, compatible with SysV and LSB init scripts."
 PKG_BUILD_FLAGS="+lto"
 
@@ -28,7 +28,7 @@ PKG_MESON_OPTS_TARGET="--libdir=/usr/lib \
                        -Dpam=disabled \
                        -Dpwquality=disabled \
                        -Dmicrohttpd=disabled \
-                       -Dlibcryptsetup=enabled \
+                       -Dlibcryptsetup=disabled \
                        -Dlibcurl=disabled \
                        -Dlibidn2=enabled \
                        -Dqrencode=disabled \
