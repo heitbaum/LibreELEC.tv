583b0b0ca2313e6f23419390f7dc051f7a1f0080
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-09 06:54:11 +0000

    nvtop: update to 3.3.2

--- packages/debug/nvtop/package.mk
+++ packages/debug/nvtop/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nvtop"
-PKG_VERSION="3.3.1"
-PKG_SHA256="8318aff973e0850bea4b9d7d313c513206cdc9b8387e8441681e84ac2bc0e980"
+PKG_VERSION="3.3.2"
+PKG_SHA256="48a295f3b3a917cc851d1aa8b185c09fde3a1b1e741fc57d7fa96b3671271630"
 PKG_LICENSE="GPL-3.0-or-later"
 PKG_SITE="https://github.com/Syllo/nvtop"
 PKG_URL="https://github.com/Syllo/nvtop/archive/${PKG_VERSION}.tar.gz"
