fd469341ccea1a27b5a639e5d02d27124a531cc5
Rudi Heitbaum <rudi@heitbaum.com>
2026-02-11 07:35:37 +0000

    spirv-llvm-translator: update to githash 18066de

--- packages/graphics/spirv-llvm-translator/package.mk
+++ packages/graphics/spirv-llvm-translator/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="spirv-llvm-translator"
-PKG_VERSION="21.1.4"
-PKG_SHA256="e900c907b62f82cf541a611b33d6685f375fe3bda6f041087fcdba350ffb437d"
+PKG_VERSION="18066de5787ffba4d5287a8b3e15c7c276271148"
+PKG_SHA256="9440d11591960f36ad82e932c5186dc7512c00732bb866dbd47da15cb0413827"
 PKG_LICENSE="LLVM"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-LLVM-Translator"
-PKG_URL="https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/v${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="SPIRV-LLVM-Translator"
 PKG_TOOLCHAIN="manual"
