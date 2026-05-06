476d442fa632d05f7e8c012abe694e48ee8e8ed1
Rudi Heitbaum <rudi@heitbaum.com>
2026-05-06 22:34:56 +0000

    libfyaml: update to githash 25540e1
    Log:
    - https://github.com/pantoniou/libfyaml/compare/25540e1ac4beb16bdf9e18441abc06071d271896...25540e1ac4beb16bdf9e18441abc06071d271896

--- packages/addons/addon-depends/flatpak-depends/libfyaml/package.mk
+++ packages/addons/addon-depends/flatpak-depends/libfyaml/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libfyaml"
-PKG_VERSION="1.0.0-alpha5"
-PKG_SHA256="aa2eada2bdddc17792708c108ed947633da21fde3124c3ebc764dfea6c257566"
+PKG_VERSION="25540e1ac4beb16bdf9e18441abc06071d271896"
+PKG_SHA256="38f6210984639a17f412f8ab50dc982c57e10873f86a51d0e6e8a03e3ee80874"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/pantoniou/libfyaml"
-PKG_URL="https://github.com/pantoniou/libfyaml/archive/v${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/pantoniou/libfyaml/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Fully feature complete YAML parser and emitter"
