6a469ff1ce7292a94e40cd8cfc19f5ccaeb57775
Rudi Heitbaum <rudi@heitbaum.com>
2026-07-05 07:05:32 +0000

    linux: update to 7.1.3

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -37,8 +37,8 @@ case "${LINUX}" in
     PKG_PATCH_DIRS="default rockchip"
     ;;
   *)
-    PKG_VERSION="7.1.2"
-    PKG_SHA256="37198c93727be247c9fb5309bb86cd5e496c61e5322cd8c4eca9476bb0b5883f"
+    PKG_VERSION="7.1.3"
+    PKG_SHA256="be41c068e88f5242a19bccdbffbe077b18c47b45f627e2325504b4fab79dd1dc"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     case ${DEVICE} in
