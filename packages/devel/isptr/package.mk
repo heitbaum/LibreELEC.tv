fcf9009e6dbe6fe5daac9f1a2668eae0f787fc52
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-02 16:42:22 +1000

    isptr: new package
    Smart pointer for intrusively reference counted objects. Build dependency of wsdd-native, consumed from source as upstream
    installs no system library.

---
+++ packages/devel/isptr/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="isptr"
+PKG_VERSION="1.13"
+PKG_SHA256="8fdad3b3e3ca1477421e0035e3aaa08986abca34e737fb30125e2c78f4ecd363"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/gershnik/intrusive_shared_ptr"
+PKG_URL="https://github.com/gershnik/intrusive_shared_ptr/archive/v${PKG_VERSION}.tar.gz"
+PKG_SOURCE_DIR="intrusive_shared_ptr-${PKG_VERSION}"
+PKG_DEPENDS_HOST=""
+PKG_LONGDESC="Intrusive shared pointer: a smart pointer for intrusively reference counted objects."
+PKG_TOOLCHAIN="manual"
