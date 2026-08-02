fe83ad8af84e87ef4ee10b326ce5a37c07e6526a
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-02 16:42:23 +1000

    modern-uuid: new package
    Modern C++ UUID library. Build dependency of wsdd-native, consumed from source as upstream
    installs no system library.

---
+++ packages/devel/modern-uuid/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="modern-uuid"
+PKG_VERSION="2.5"
+PKG_SHA256="f92fc9b62293049a898ca3003d0a1e416b56cdf3205a986a0a5426016dd6b000"
+PKG_LICENSE="BSD-3-Clause"
+PKG_SITE="https://github.com/gershnik/modern-uuid"
+PKG_URL="https://github.com/gershnik/modern-uuid/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST=""
+PKG_LONGDESC="modern-uuid: a modern C++ UUID library."
+PKG_TOOLCHAIN="manual"
