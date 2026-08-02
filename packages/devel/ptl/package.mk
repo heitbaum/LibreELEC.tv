256c0c03e47a8027558a7f9a11accfd1f13c024f
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-02 16:42:24 +1000

    ptl: new package
    C++ wrapper for POSIX and Linux APIs. Build dependency of wsdd-native, consumed from source as upstream
    installs no system library.

---
+++ packages/devel/ptl/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="ptl"
+PKG_VERSION="1.11"
+PKG_SHA256="e3c7e954936953413d1b4cf863912f4324f8085b1b80eaad795ff09dc10691a1"
+PKG_LICENSE="BSD-3-Clause"
+PKG_SITE="https://github.com/gershnik/ptl"
+PKG_URL="https://github.com/gershnik/ptl/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST=""
+PKG_LONGDESC="PTL: a C++ wrapper for POSIX and Linux APIs."
+PKG_TOOLCHAIN="manual"
