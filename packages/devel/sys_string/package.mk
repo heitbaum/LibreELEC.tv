252b3786fba59ec15c976f32ce5f72f56430e8b0
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-02 16:42:24 +1000

    sys_string: new package
    Immutable string class wrapping the platform native string type. Build dependency of wsdd-native, consumed from source as upstream
    installs no system library.

---
+++ packages/devel/sys_string/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="sys_string"
+PKG_VERSION="3.14"
+PKG_SHA256="66734cb6b217326725e974cf1e76348bbad2cfa0e33962a66438ad3889917065"
+PKG_LICENSE="BSD-3-Clause"
+PKG_SITE="https://github.com/gershnik/sys_string"
+PKG_URL="https://github.com/gershnik/sys_string/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST=""
+PKG_LONGDESC="sys_string: an immutable string class that wraps the native string type of the platform."
+PKG_TOOLCHAIN="manual"
