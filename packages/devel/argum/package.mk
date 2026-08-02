ca98d29ac55a8184f3b50e741d5df4be6d978def
Rudi Heitbaum <rudi@heitbaum.com>
2026-08-02 16:42:22 +1000

    argum: new package
    C++ command line parsing library. Build dependency of wsdd-native, consumed from source as upstream
    installs no system library.

---
+++ packages/devel/argum/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2026-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="argum"
+PKG_VERSION="2.11"
+PKG_SHA256="b64630ee57df3487bac6a3f4baaf9fcb9ce99becb5e5a21def5f31effe30791f"
+PKG_LICENSE="BSD-3-Clause"
+PKG_SITE="https://github.com/gershnik/argum"
+PKG_URL="https://github.com/gershnik/argum/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_HOST=""
+PKG_LONGDESC="Argum: C++ command line parsing library."
+PKG_TOOLCHAIN="manual"
