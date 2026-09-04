bdf7def718acb779f4645ab79a9f4eec098f3911
Rudi Heitbaum <rudi@heitbaum.com>
2026-09-04 13:06:57 +0000

    linux: update to 7.2.3

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -37,8 +37,8 @@ case "${LINUX}" in
     PKG_PATCH_DIRS="default rockchip"
     ;;
   *)
-    PKG_VERSION="7.2.1"
-    PKG_SHA256="4158098c8a28ea8fbe068c855d3aa6341e03d06282add3cb9049f3a24e4557b3"
+    PKG_VERSION="7.2.3"
+    PKG_SHA256="8ba259e8e7b13ec6ef0941c8a39ad90b24bd4a4d6c0010ba6bafb794550ecd03"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v${PKG_VERSION/.*/}.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     case ${DEVICE} in
