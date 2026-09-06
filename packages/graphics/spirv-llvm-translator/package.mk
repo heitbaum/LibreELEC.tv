610dde3dc4997a24063777d607416d5fce76a475
Rudi Heitbaum <rudi@heitbaum.com>
2026-09-06 09:03:25 +0000

    Revert "spirv-llvm-translator: update to 23.1.1"
    This reverts commit 4014e68eb9da1f8d964eb2d48c589086dbbb7a4c.

--- packages/graphics/spirv-llvm-translator/package.mk
+++ packages/graphics/spirv-llvm-translator/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="spirv-llvm-translator"
-PKG_VERSION="23.1.1"
-PKG_SHA256="9e0bf1beb0ab7edca6cd8c17fd9bbb7ce3dcd520f3e969e0485ff5b00931f929"
+PKG_VERSION="22.1.5"
+PKG_SHA256="3c6dffb4b8d67f5c544370e0c869ec7d22c013d4bb798f24655ec903f26cc5d5"
 PKG_LICENSE="NCSA"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-LLVM-Translator"
 PKG_URL="https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/v${PKG_VERSION}.tar.gz"
