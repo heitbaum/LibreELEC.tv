401ee99bfde54e47769a6bb57417a241628cdfdc
Rudi Heitbaum <rudi@heitbaum.com>
2026-09-06 09:03:28 +0000

    Revert "llvm: update to 23.1.0"
    This reverts commit c326a0788023b3ed33a94f57ce552de93565164f.

--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="llvm"
-PKG_VERSION="23.1.0"
-PKG_SHA256="ab1f0e3ec52448c33e8782eaf0422504b87c7b016b22514653ee0d8fcee479ff"
+PKG_VERSION="22.1.8"
+PKG_SHA256="922f1817a0df7b1489272d18134ee0087a8b068828f87ac63b9861b1a9965888"
 PKG_LICENSE="Apache-2.0 WITH LLVM-exception"
 PKG_SITE="http://llvm.org/"
 PKG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_VERSION}/llvm-project-${PKG_VERSION/-/}.src.tar.xz"
